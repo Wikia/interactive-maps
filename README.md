2d3884d9c3f1b68b76d9ff88ad9e8cf7c1c58dab
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-03-03 10:22:16 +0100

    Start writing logic for adding a map to DB and cutting it into tiles

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ Interactive Maps
 ================
 
 Service that allows creating, storing and serving interactive maps
+
+
+# Installation
+
+# What it is
+
+# What it isn't
--- server.js
+++ server.js
@@ -4,11 +4,13 @@ var Percolator = require('percolator').Percolator,
 	connect = require('connect'),
 	server = new Percolator({autoLink: false});
 
-server.connectMiddleware( connect.logger({ format:'dev'}));
+server.connectMiddleware(connect.logger({ format: 'dev'}));
 
-server.routeDirectory(__dirname + '/resources', '/api', function(err){
-	if (!!err) {console.log(err);}
-	server.listen(function(){
+server.routeDirectory(__dirname + '/resources', '/api', function (err) {
+	if (!!err) {
+		console.log(err);
+	}
+	server.listen(function () {
 		console.log('server is listening on port ', server.port)
 	});
 });
