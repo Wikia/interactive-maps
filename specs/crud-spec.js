6d38413a8baa9481fe02db272ddda3aa5009bc8e
aquilax <evgeniy@wikia-inc.com>
2014-03-21 15:48:31 +0100

    Added unit tests; created utils module for functions

---
+++ specs/crud-spec.js
@@ -0,0 +1,37 @@
+curd = require('../lib/curd');
+
+describe('crud', function(){
+
+	var config = {
+		dbTable: 'test',
+		dbColumns: 'test',
+		customMethods: {
+			'create': function() {},
+			'update': function() {}
+		},
+		createSchema: {},
+		updateSchema: {}
+	};
+
+	it('throws an exteption when config is not provided', function(){
+		expect(
+			function() {
+				curd.createCollection()
+			})
+			.toThrow(new Error('Config should be an object!'));
+	});
+
+	it('throws an exteption when config.dbTable is not set', function(){
+		expect(
+			function() {
+				curd.createCollection({});
+			})
+			.toThrow(new Error('Config properties "dbTable" and "dbColumns" are required!'));
+	});
+
+	it('creates curd object', function(){
+		var curdObj = curd.createCollection(config)
+		expect(typeof curdObj).toBe('object');
+	});
+
+});
