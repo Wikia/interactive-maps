5824e4d92802c5447aa2063547f984d3880d6697
aquilax <evgeniy@wikia-inc.com>
2014-03-26 16:38:11 +0100

    Added fetchImage spec

--- specs/fetchImage-spec.js
+++ specs/fetchImage-spec.js
@@ -7,7 +7,39 @@ describe('Fetch image', function() {
         expect(fetchImage()).toThrow(new Error('Required data not set'));
     });
 
-    it('Calls http.get to download the image', function() {
-        var fetchImage = proxyquire('../lib/fetchImage', {});
+    it('calls http.get to download the image', function() {
+        var res = createSpyObj('res', ['on']),
+            http = {
+                get: function(x, cb) {
+                    cb({
+                        on: function() {
+                            return res;
+                        }
+                    });
+                    return {
+                        on: function() {}
+                    }
+                }
+            },
+            defer = createSpyObj('defer', ['resolve', 'reject', 'promise']),
+            fsStub = createSpyObj('fs', ['createWriteStream']),
+            QStub = {
+                defer: function() {
+                    return defer
+                }
+            },
+            fetchImage = proxyquire('../lib/fetchImage', {
+                http: http,
+                url: require('url'),
+                fs: fsStub,
+                path: {},
+                q: QStub
+            }),
+            data = {
+                fileUrl: 'http:/example.com/image.jpg'
+            };
+        fetchImage(data);
+        expect(res.on).toHaveBeenCalled();
+        expect(fsStub.createWriteStream).toHaveBeenCalled();
     });
 });
