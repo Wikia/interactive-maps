fda134913b37d2ee15546210f5afe6d552a2ad8e
aquilax <evgeniy@wikia-inc.com>
2014-03-27 14:18:39 +0100

    Started dfs spec

---
+++ specs/dfs-spec.js
@@ -0,0 +1,30 @@
+var proxyquire = require('proxyquire').noCallThru(),
+	stubs = require('./stubs');
+
+describe('DFS', function (){
+
+	it('Throws error on wrong params', function() {
+		var dfs = proxyquire('../lib/dfs', {});
+		expect(dfs.sendFiles()).toThrow(new Error('Required data not set'));
+	})
+
+	it('Uploads files', function(){
+		var dfs = proxyquire('../lib/dfs', {
+				'./config': {
+					swift: {
+						host: '',
+						authPath: '',
+						user: '',
+						key: ''
+					}
+				}
+			}),
+			data = {
+				bucket: '',
+				dir: '',
+				filePaths: ''
+			}
+		dfs.sendFiles(data.bucket, data.dir, data.filePaths);
+	})
+
+});
