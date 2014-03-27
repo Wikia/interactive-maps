149375c2cd1e5405e3df1efc1ca6455e2bfbefbe
aquilax <evgeniy@wikia-inc.com>
2014-03-27 14:25:03 +0100

    Fixed missing semicolumns

--- specs/cleanupTiles-spec.js
+++ specs/cleanupTiles-spec.js
@@ -1,51 +1,51 @@
 var proxyquire = require('proxyquire').noCallThru(),
-    stubs = require('./stubs')
+    stubs = require('./stubs');
 
-    describe('Clean tiles', function() {
+describe('Clean tiles', function() {
 
-        var qStub,
-            childProcessMock;
-
-
-        beforeEach(function() {
-            qStub = stubs.newQStub();
-            childProcessMock = createSpyObj('child_process', ['exec'])
-        });
-
-        it('does not run, if cleanup is disabled by config', function() {
-            var cleanupTilesDisabled = proxyquire('../lib/cleanupTiles', {
-                q: qStub.q,
-                child_process: childProcessMock,
-                './config': {
-                    cleanup: false
-                }
-            }),
-                data = {
-                    dir: 'fake_dir'
-                };
-            cleanupTilesDisabled(data);
-            expect(childProcessMock.exec).not.toHaveBeenCalled();
-            expect(qStub.defer.resolve).toHaveBeenCalledWith(data);
-            expect(qStub.defer.resolve.callCount).toEqual(1);
-        })
-
-        it('executes rm with correct parameters', function() {
-            var cleanupTilesDisabled = proxyquire('../lib/cleanupTiles', {
-                q: qStub.q,
-                child_process: childProcessMock,
-                './config': {
-                    cleanup: true
-                }
-            }),
-                data = {
-                    dir: 'fake_dir',
-                    minZoom: 1,
-                    maxZoom: 2
-                },
-                expected = 'rm -rf ' + data.dir + '/{' + data.minZoom + '..' + data.maxZoom + '}';
-            cleanupTilesDisabled(data);
-            expect(childProcessMock.exec).toHaveBeenCalledWith(expected);
-        })
+    var qStub,
+        childProcessMock;
 
 
+    beforeEach(function() {
+        qStub = stubs.newQStub();
+        childProcessMock = createSpyObj('child_process', ['exec'])
     });
+
+    it('does not run, if cleanup is disabled by config', function() {
+        var cleanupTilesDisabled = proxyquire('../lib/cleanupTiles', {
+            q: qStub.q,
+            child_process: childProcessMock,
+            './config': {
+                cleanup: false
+            }
+        }),
+            data = {
+                dir: 'fake_dir'
+            };
+        cleanupTilesDisabled(data);
+        expect(childProcessMock.exec).not.toHaveBeenCalled();
+        expect(qStub.defer.resolve).toHaveBeenCalledWith(data);
+        expect(qStub.defer.resolve.callCount).toEqual(1);
+    })
+
+    it('executes rm with correct parameters', function() {
+        var cleanupTilesDisabled = proxyquire('../lib/cleanupTiles', {
+            q: qStub.q,
+            child_process: childProcessMock,
+            './config': {
+                cleanup: true
+            }
+        }),
+            data = {
+                dir: 'fake_dir',
+                minZoom: 1,
+                maxZoom: 2
+            },
+            expected = 'rm -rf ' + data.dir + '/{' + data.minZoom + '..' + data.maxZoom + '}';
+        cleanupTilesDisabled(data);
+        expect(childProcessMock.exec).toHaveBeenCalledWith(expected);
+    })
+
+
+});
--- specs/fetchImage-spec.js
+++ specs/fetchImage-spec.js
@@ -1,41 +1,41 @@
 var proxyquire = require('proxyquire').noCallThru(),
-    stubs = require('./stubs')
+    stubs = require('./stubs');
 
-    describe('Fetch image', function() {
+describe('Fetch image', function() {
 
-        it('throws an exception on empty data', function() {
-            var fetchImage = proxyquire('../lib/fetchImage', {});
-            expect(fetchImage()).toThrow(new Error('Required data not set'));
-        });
-
-        it('calls http.get to download the image', function() {
-            var res = createSpyObj('res', ['on']),
-                http = {
-                    get: function(x, cb) {
-                        cb({
-                            on: function() {
-                                return res;
-                            }
-                        });
-                        return {
-                            on: function() {}
-                        }
-                    }
-                },
-                qStub = stubs.newQStub(),
-                fsStub = createSpyObj('fs', ['createWriteStream']),
-                fetchImage = proxyquire('../lib/fetchImage', {
-                    http: http,
-                    url: require('url'),
-                    fs: fsStub,
-                    path: {},
-                    q: qStub.q
-                }),
-                data = {
-                    fileUrl: 'http:/example.com/image.jpg'
-                };
-            fetchImage(data);
-            expect(res.on).toHaveBeenCalled();
-            expect(fsStub.createWriteStream).toHaveBeenCalled();
-        });
+    it('throws an exception on empty data', function() {
+        var fetchImage = proxyquire('../lib/fetchImage', {});
+        expect(fetchImage()).toThrow(new Error('Required data not set'));
     });
+
+    it('calls http.get to download the image', function() {
+        var res = createSpyObj('res', ['on']),
+            http = {
+                get: function(x, cb) {
+                    cb({
+                        on: function() {
+                            return res;
+                        }
+                    });
+                    return {
+                        on: function() {}
+                    }
+                }
+            },
+            qStub = stubs.newQStub(),
+            fsStub = createSpyObj('fs', ['createWriteStream']),
+            fetchImage = proxyquire('../lib/fetchImage', {
+                http: http,
+                url: require('url'),
+                fs: fsStub,
+                path: {},
+                q: qStub.q
+            }),
+            data = {
+                fileUrl: 'http:/example.com/image.jpg'
+            };
+        fetchImage(data);
+        expect(res.on).toHaveBeenCalled();
+        expect(fsStub.createWriteStream).toHaveBeenCalled();
+    });
+});
