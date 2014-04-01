2d88de3d1c0386fea0c476ce1a1fb3cf2e37ab02
aquilax <evgeniy@wikia-inc.com>
2014-04-01 15:11:57 +0200

    Added sample POI

--- assets/scripts/map.js
+++ assets/scripts/map.js
@@ -6,6 +6,8 @@
 		apiBaseDirectory = 'api',
 		apiVersion = 'v1',
 		mapContainerId = 'map',
+		imagePath = '/images',
+		map,
 
 		// TODO: remove this once we return all required info from the API
 		defaultMapConfig = {
@@ -13,7 +15,15 @@
 			initLon: 0,
 			initZoom: 0,
 			pathTemplate: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-			mapSetup: {}
+			mapSetup: {},
+			points: [
+				{
+					title: 'test name',
+					description: 'test description',
+					lat: 42.676845,
+					lon: 23.283205
+				}
+			]
 		};
 
 	function createUrl(request) {
@@ -40,16 +50,28 @@
 		// TODO: Add points to the map
 	}
 
-	function createMap(config) {
-		var map;
+	function addPointOnMap(point) {
+		var popupHtml = '<h3>' + point.title + '</h3>' +
+			'<p>' +point.description + '</p>';
+		return L.marker([ point.lat, point.lon ], {
+			riseOnHover: true
+		})
+			.bindPopup(popupHtml)
+			.addTo(map);
+	}
 
+	function createMap(config) {
 		config = $.extend(true, config, defaultMapConfig);
+		L.Icon.Default.imagePath = imagePath;
 		map = L.map(mapContainerId)
 			.setView([config.initLat, config.initLon], config.initZoom);
 
 		L.tileLayer(config.pathTemplate, config.mapSetup).addTo(map);
 
-		createPoints(config);
+		config.points.forEach(function (point){
+			addPointOnMap(point);
+		});
+
 	}
 
 	function init(){
