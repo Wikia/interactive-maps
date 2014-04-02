72b8e5c6264864bb9ddf64c4bd0f2e1d6e723206
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-04-02 12:23:43 +0200

    simple test for jobprocessors

--- specs/jobProcessors-spec.js
+++ specs/jobProcessors-spec.js
@@ -4,11 +4,26 @@ var proxyquire = require('proxyquire').noCallThru(),
 	stubs = require('./stubs');
 
 describe('jobProcessors', function () {
-	it('should process a process job', function () {
-
-	});
-
-	it('should process a tiling job', function () {
-
+	it('should process jobs', function () {
+		proxyquire('../lib/jobProcessors', {
+			'./config': {
+				kue: {
+					maxFetchJobs: 99,
+					maxCutTilesJobs: 99
+				}
+			},
+			'kue': {
+				createQueue: function(){
+					return {
+						process: function(name, maxJobs, runner){
+							expect(name).toMatch(/process|tiling/);
+							expect(maxJobs).toBe(99);
+							expect(typeof runner).toBe('function');
+						}
+					};
+				}
+			},
+			'./db_connector': {}
+		});
 	});
 });
