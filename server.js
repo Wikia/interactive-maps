acf32a8621ce205fd1d8bf898ce824870bf58860
bkoval <brt.kowal@gmail.com>
2014-04-02 12:56:29 +0200

    Abstracting date and js-beautify

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var Percolator = require('percolator').Percolator,
 
 server.connectMiddleware(connect.logger({ format: 'dev'}));
 
+var logger = require('./lib/logger').getInstance();
+logger.set({console: {level: 7, raw: true, enabled: true}});
+logger.debug('welcome');
+
 server.routeDirectory(__dirname + '/resources', '/api', function (err) {
 	if (!!err) {
 		console.log(err);
