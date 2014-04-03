db3cd02ca77e5fd4b1099e010b7e90e23c3c6c66
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-04-03 09:15:01 +0200

    move newrelic config to config repo

--- newrelic.js
+++ newrelic.js
@@ -4,21 +4,23 @@
  * See lib/config.defaults.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
+var config = require('./lib/config').newrelic;
+
 exports.config = {
     /**
      * Array of application names.
      */
-    app_name: ['Interactive Maps Service'],
+    app_name: [config.appName],
     /**
      * Your New Relic license key.
      */
-    license_key: 'license key here',
+    license_key: config.licenceKey,
     logging: {
         /**
          * Level at which to log. 'trace' is most useful to New Relic when diagnosing
          * issues with the agent, 'info' and higher will impose the least overhead on
          * production applications.
          */
-        level: 'trace'
+        level: config.logLevel
     }
 };
