d2fb039601ba85443f52568b71e0d4e797709f05
aquilax <evgeniy@wikia-inc.com>
2014-04-07 15:09:57 +0200

    MOB-1355: Unit test for validateCrudConfig

---
+++ specs/validateCurdConfig-spec.js
@@ -0,0 +1,110 @@
+'use strict';
+
+describe('Validate Crud config', function () {
+
+	var vcc = require('../lib/validateCurdConfig');
+
+	it('Throws error if config is not an object', function () {
+		var testCases = [
+			1,
+			1.2,
+			'',
+			false,
+			null
+		];
+		testCases.forEach(function (testCase) {
+			expect(function () {
+				vcc(testCase);
+			}).toThrow(new Error('Config must be an object!'));
+		});
+	});
+
+	it('Throws an error if dbTable and dbColumns are not set', function () {
+		var testCases = [{}, {
+			dbTable: ''
+		}, {
+			dbColumns: ''
+		}];
+		testCases.forEach(function (testCase) {
+			expect(function () {
+				vcc(testCase);
+			}).toThrow(new Error('Config properties "dbTable" and "dbColumns" are required!'));
+		});
+	});
+
+	it('Throws an error if createSchema is not set and handler.POST is not disabled', function () {
+		var testCases = [{
+			dbTable: '',
+			dbColumns: '',
+			blockedMethods: {
+				handler: {
+					POST: true
+				}
+			}
+		}, {
+			dbTable: '',
+			dbColumns: '',
+			blockedMethods: {
+				handler: {
+					POST: true
+				}
+			},
+			createSchema: false
+		}];
+		testCases.forEach(function (testCase) {
+			expect(function () {
+				vcc(testCase);
+			}).toThrow(new Error('Schema for validating object creation is required!'));
+		});
+	});
+
+	it('Throws an error if createSchema is not set and handler.POST is not disabled', function () {
+		var testCases = [{
+			dbTable: '',
+			dbColumns: '',
+			blockedMethods: {
+				handler: {
+					POST: false
+				},
+				wildcard: {
+					PUT: true
+				}
+			}
+		}, {
+			dbTable: '',
+			dbColumns: '',
+			blockedMethods: {
+				handler: {
+					POST: false
+				},
+				wildcard: {
+					PUT: true
+				}
+			},
+			updateSchema: false
+		}];
+		testCases.forEach(function (testCase) {
+			expect(function () {
+				vcc(testCase);
+			}).toThrow(new Error('Schema for validating object updating is required!'));
+		});
+	});
+
+	it('Return undefined if all conditions are met', function () {
+		var testCase = {
+			dbTable: '',
+			dbColumns: '',
+			blockedMethods: {
+				handler: {
+					POST: false
+				},
+				wildcard: {
+					PUT: true
+				}
+			},
+			createSchema: {},
+			updateSchema: {}
+		};
+		expect(typeof vcc(testCase)).toBe('undefined');
+	});
+});
