95ccf3678eca3bfd72b9bd95045c6b7662291a84
Rafal Leszczynski <rafal@wikia-inc.com>
2014-04-08 14:59:25 +0200

    MOB-1358, unit test for json validator

---
+++ specs/jsonValidator-spec.js
@@ -0,0 +1,55 @@
+'use strict';
+
+describe('json validator', function() {
+	var proxyquire = require('proxyquire').noCallThru(),
+		jsonValidator = proxyquire('./../lib/jsonValidator', {
+			'JSV': {
+				JSV: {
+					createEnvironment: function() {
+						return {
+							validate: function() {
+								return {
+									errors: true
+								};
+							}
+						}
+					}
+				}
+			}
+		});
+
+	it('returns "Incorrect JSON format!" error', function() {
+		var testCases = [
+			'bla bla bla',
+			1,
+			null,
+			{},
+			[],
+			undefined,
+			NaN
+		];
+
+		testCases.forEach(function(value) {
+			var errors = jsonValidator(value);
+
+			expect(Array.isArray(errors)).toBeTruthy();
+			expect(errors.length).toBe(1);
+			expect(errors[0].message).toBe('Incorrect JSON format!');
+		});
+	});
+
+	it('validate proper JSON against given schema', function() {
+		var testCases = [
+			{
+				test: 1
+			},
+			[1,2,3]
+		];
+
+		testCases.forEach(function(value) {
+			var errors = jsonValidator(value);
+
+			expect(errors).toBe(true);
+		});
+	});
+});
