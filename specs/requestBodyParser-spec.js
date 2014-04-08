07b3828e6dddec0729c4782b186dd3e4aaf742c4
Rafal Leszczynski <rafal@wikia-inc.com>
2014-04-08 16:22:38 +0200

    fix typos

--- specs/requestBodyParser-spec.js
+++ specs/requestBodyParser-spec.js
@@ -3,7 +3,7 @@
 describe('Request Body Parser', function () {
 	var requestBodyParser = require('./../lib/requestBodyParser.js');
 
-	it('parse raw request body to JSON', function() {
+	it('parses raw request body to JSON', function() {
 		var testCases = [
 			'{"message": "Pink unicorns flying!"}',
 			'{}',
@@ -17,7 +17,7 @@ describe('Request Body Parser', function () {
 		});
 	});
 
-	it('return raw body if JSON.parse() throws error', function() {
+	it('returns raw body if JSON.parse() throws error', function() {
 		var rawBody = '{"message: "Pink unicorns flying!"}';
 
 		expect(typeof requestBodyParser(rawBody) === 'string').toBeTruthy();
