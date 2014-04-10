3a6a7009e15dac62bb7ed116a45c612acbfa6747
aquilax <evgeniy@wikia-inc.com>
2014-04-10 12:38:10 +0200

    Moved the main logic to lib module; Added unitTest

---
+++ specs/healthCheck-spec.js
@@ -0,0 +1,75 @@
+'use strict';
+
+describe('Health Check', function() {
+
+	var proxyquire = require('proxyquire').noCallThru(),
+		error = null,
+		queueSize = 20,
+		healthCheck = proxyquire('./../lib/healthCheck', {
+			'./config': {},
+			'kue': {
+				createQueue: function(){
+					return {
+						inactiveCount: function(callback) {
+							callback(error, queueSize);
+						}
+					};
+				}
+			}
+		});
+
+	it('is module', function() {
+		expect(typeof healthCheck).toBe('object');
+	});
+
+	it('exports exit codes', function() {
+		expect(typeof healthCheck.exitCodes).toBe('object');
+	});
+
+	it('to return proper result code depending on the thresholds', function() {
+		var testCases  = [
+			{
+				thresholds: {
+					20: healthCheck.exitCodes.OK
+				},
+				expectedCode: healthCheck.exitCodes.OK
+			},
+			{
+				thresholds: {
+					10: healthCheck.exitCodes.OK,
+					20: healthCheck.exitCodes.WARNING
+				},
+				expectedCode: healthCheck.exitCodes.WARNING
+			},
+			{
+				thresholds: {
+					5: healthCheck.exitCodes.OK,
+					10: healthCheck.exitCodes.WARNING,
+					20: healthCheck.exitCodes.CRITICAL
+				},
+				expectedCode: healthCheck.exitCodes.CRITICAL
+			}
+		];
+		testCases.forEach(function(testCase){
+			healthCheck.getQueueSize(
+				testCase.thresholds,
+				function (result) {
+					expect(result.code).toBe(testCase.expectedCode);
+				}
+			);
+		});
+	});
+
+	if ('returns unknown status if error arises', function(){
+		var error = {
+			message: 'You shall not pass'
+		};
+		healthCheck.getQueueSize(
+			{},
+			function (result) {
+				expect(result.code).toBe(healthCheck.exitCodes.UNKNOWN);
+				expect(result.message).toBe(error.message);
+			}
+		);
+	});
+});
---
+++ tools/nagiosQueueSize.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var healthCheck = require(__dirname + '/../lib/healthCheck'),
+	// TODO: These values are currently just wild guesses, change them when we have some tests
+	inactiveThresholds = {
+		20: healthCheck.exitCodes.OK,
+		40: healthCheck.exitCodes.WARNING,
+		60: healthCheck.exitCodes.CRITICAL
+	};
+
+function printResult(result) {
+	console.log(result.message);
+	process.exit(result.code);
+}
+
+healthCheck.getQueueSize( inactiveThresholds, printResult );
