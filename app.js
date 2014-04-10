1f05931b5edb4436afc704912a5150149e6c0406
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-04-10 16:51:01 +0200

    move shutdown of a node to app so it is only called on master

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ var logger = require('./lib/logger'),
 	cluster = require('cluster'),
 	numCPUs = require('os').cpus().length,
 	fs = require('fs'),
-	config;
+	config,
+	kue = require('kue'),
+	jobs;
 
 logger.set({
 	console: {
@@ -22,6 +24,47 @@ config = require('./lib/config');
 
 config.setRoot(__dirname);
 
+jobs = kue.createQueue(config);
+
+/**
+ * @desc Called on exit to cleanup kue and close it
+ */
+function shutdown(){
+	jobs.shutdown(function () {
+		logger.debug('Jobs cleaned up and set to delayed states');
+		process.exit(0);
+	}, 1000);
+}
+
+/**
+ * @desc called on SIGINT or SIGTERM to deactivate jobs
+ */
+function onDie() {
+	//when terminating: mark all active jobs as delayed
+	//so kue can pick them up after restart
+	jobs.active(function (err, ids) {
+		var length = ids.length,
+			count = 0,
+			i = 0,
+			delayJob = function (err, job) {
+				job.delayed().save();
+				count++;
+
+				if (count === length) {
+					shutdown();
+				}
+			};
+
+		if (length) {
+			for (; i < length; i++) {
+				kue.Job.get(ids[i], delayJob);
+			}
+		} else {
+			shutdown();
+		}
+	});
+}
+
 if (cluster.isMaster) {
 
 	// Fork workers.
@@ -33,6 +76,9 @@ if (cluster.isMaster) {
 	if (!fs.existsSync(config.tmp)) {
 		fs.mkdirSync(config.tmp);
 	}
+
+	process.on('SIGINT', onDie);
+	process.on('SIGTERM', onDie);
 
 	require('./apiServer');
 } else {
