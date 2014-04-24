d5344b2fac30cb7209e88c9bf359f3a230982fca
Rafal Leszczynski <rafal@wikia-inc.com>
2014-04-24 15:44:48 +0200

    fix unit tests for optimize tiles

--- specs/optimizeTiles-spec.js
+++ specs/optimizeTiles-spec.js
@@ -3,37 +3,82 @@
 var proxyquire = require('proxyquire').noCallThru(),
 	stubs = require('./stubs');
 
-xdescribe('Optimize tiles', function () {
+describe('Optimize tiles', function () {
+    var execSpy = jasmine.createSpy('execSpy'),
+        loggerSpy = jasmine.createSpy('loggerSpy'),
+        childProcessMock = {
+            exec: execSpy
+        };
 
-	it('throws an error on incorrect data', function () {
-		var optimizeTiles = proxyquire('../lib/optimizeTiles', {
+    /**
+     * @desc Require optimize tiles module with mocked dependencies
+     *
+     * @param {boolean} isEnabled - flag to enable or disable optimizing
+     *
+     * @returns {object} - optimize tiles module
+     */
 
+    function createOptimizeTilesMock(isEnabled) {
+        return proxyquire('../lib/optimizeTiles', {
+            child_process: childProcessMock,
+            './config': {
+                optimize: isEnabled
+            },
+            './utils': {
+                getGlob: function() {}
+            },
+            './logger': {
+                info: loggerSpy,
+                notice: loggerSpy,
+                error: function() {}
+            }
         });
-		expect(optimizeTiles()).toThrow(new Error('Required data not set'));
-	});
+    }
+
+    /**
+     * @desc create job mock
+     *
+     * @param {boolean} isOptimized - flag set to true if optimizing job already completed
+     *
+     * @returns {object} - job mock
+     */
+
+    function createJobMock(isOptimized) {
+        return {
+            data: {
+                dir: 'test',
+                status: {
+                    optimized: isOptimized
+                }
+            }
+        };
+    }
+
 
 	it('executes the tile optimization process', function () {
-		var streamStub = new createSpyObj('stream', ['trim']),
-			collector = stubs.newCollector(['run']),
-			childProcessMock = {
-				exec: function (cmd, cb) {
-					collector.run(cmd);
-					cb(streamStub, streamStub, streamStub);
-				}
-			},
-			optimizeTiles = proxyquire('../lib/optimizeTiles', {
-				child_process: childProcessMock,
-				'./config': {
-					optimize: true
-				}
-			}),
-			data = {
+        var optimizeTiles = createOptimizeTilesMock(true),
+            job = createJobMock(false);
 
-			};
+        optimizeTiles(job);
 
-		optimizeTiles(data);
-
-		expect(collector.run.callCount).toEqual(3);
-		expect(streamStub.trim).toHaveBeenCalled();
+		expect(execSpy).toHaveBeenCalled();
 	});
+
+    it('doesn\'t execute the tile optimization process if tiles are already optimized', function() {
+        var optimizeTiles = createOptimizeTilesMock(true),
+            job = createJobMock(true);
+
+        optimizeTiles(job);
+
+        expect(loggerSpy).toHaveBeenCalledWith('Tiles already optimized');
+    });
+
+    it('doesn\'t execute the tile optimization process if it\'s disabled', function() {
+        var optimizeTiles = createOptimizeTilesMock(false),
+            job = createJobMock(false);
+
+        optimizeTiles(job);
+
+        expect(loggerSpy).toHaveBeenCalledWith('Optimizing images disabled');
+    });
 });
