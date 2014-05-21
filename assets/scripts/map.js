e4fc898f7e9bddf6ef2a934ac5bdaef7fc9f48e2
igor <igor@wikia-inc.com>
2014-05-21 13:45:35 -0700

    Cleaned up after rebase from master/origin

--- assets/scripts/map.js
+++ assets/scripts/map.js
@@ -3,15 +3,23 @@
 
 	var mapContainerId = 'map',
 		map,
+		pointTypesContainerId = 'point_types',
+		pointTypeFiltersContainer,
 		popupWidthWithPhoto = 414,
 		popupWidthWithoutPhoto = 314,
 		photoWidth = 90,
-		photoHeight = 90;
+		photoHeight = 90,
+		pointIconWidth = 32,
+		pointIconHeight = 32,
+		pointIcons = {},
+		pointCache = {},
+		pointTypes = {};
+
 
 	/**
 	 * @desc Build popup HTML
 	 * @param point {object} - POI object
-	 * @returns {string} - Popup HTML makup
+	 * @returns {string} - HTML markup for popup
 	 */
 	function buildPopupHtml(point) {
 		//TODO what about edit link? where do we get it from?
@@ -42,9 +50,9 @@
 
 	/**
 	 * @desc Build photo HTML
-	 * @param photoUrl {string} - image URL
-	 * @param alt {string} - image alternate text
-	 * @returns {string} Photo HTML
+	 * @param photoUrl {string} - Image URL
+	 * @param alt {string} - Image alternate text
+	 * @returns {string} - HTML markup for photo
 	 */
 	function buildPhotoHtml(photoUrl, alt) {
 		//TODO what about that width and height?
@@ -71,23 +79,178 @@
 
 	/**
 	 * @desc Add point to the map
-	 * @param point {object}
-	 * @returns {object} Leaflet Marker
+	 * @param point {object} - POI object
+	 * @returns {object} - Leaflet Marker
 	 */
 	function addPointOnMap(point) {
-		var popupHtml = '<h3>' + point.name + '</h3>' +
-			'<p>' +point.description + '</p>';
-		return L.marker([ point.lat, point.lon ], {
-			riseOnHover: true
-		})
-			.bindPopup(popupHtml)
-			.addTo(map);
+		var popup, popupWidth;
+
+		popupWidth = (point.photo) ? popupWidthWithPhoto : popupWidthWithoutPhoto;
+
+		popup = L
+			.popup({
+				closeButton: false,
+				minWidth: popupWidth,
+				maxWidth: popupWidth
+			})
+			.setContent(buildPopupHtml(point));
+
+		return L.marker([point.lat, point.lon], {
+			icon: pointIcons[point.poi_category_id],
+			riseOnHover: true,
+			type: point.type
+		}).bindPopup(popup).addTo(map);
 	}
 
-	function buildLink(link, content) {
-		if (link.length) {
-			return '<a href="' + link + '" target="_blank">' + content + '</a>';
+	/**
+	 * @desc Add filter for all point types
+	 */
+	function addAllPointsFilter() {
+		var pointsToggleHtml = '<li id="point_type_all" class="point_type enabled" data-point-type="0">' +
+			'All Pin Types' + //TODO what about translation?
+			'</li>';
+		pointTypeFiltersContainer.innerHTML += pointsToggleHtml;
+	}
+
+	/**
+	 * @desc Add filter for given point type and set its icon
+	 * @param pointType {object} - POI type object
+	 */
+	function addPointType(pointType) {
+		// if you change this structure then check if bindPointTypeFilters() function is still working
+		var pointTypeHtml = '<li class="point_type enabled" data-point-type="' + pointType.id + '">' +
+			'<img src="' + pointType.marker + '" width="' + pointIconWidth + '" height="' + pointIconHeight + '">' +
+			'<span>' + pointType.name + '</span>' +
+			'</li>';
+		pointTypeFiltersContainer.innerHTML += pointTypeHtml;
+
+		pointIcons[pointType.id] = L.icon({
+			iconUrl: pointType.marker,
+			iconSize: [pointIconWidth, pointIconHeight],
+			className: 'point-type-' + pointType.id
+		});
+	}
+
+	/**
+	 * @desc Return DOM elements for given point type, store them in cache
+	 * @param pointType {number} - Id of point type, 0 for all types
+	 * @returns {NodeList} - List of DOM elements corresponding with given point type
+	 */
+	function getPointsByType(pointType) {
+		if (typeof pointCache[pointType] === 'undefined') {
+			if (pointType === 0) {
+				pointCache[pointType] = document.getElementsByClassName('leaflet-marker-icon');
+			} else {
+				pointCache[pointType] = document.getElementsByClassName('point-type-' + pointType);
+			}
 		}
+
+		return pointCache[pointType];
+	}
+
+	/**
+	 * @desc Mark filters for given point type as disabled
+	 * @param filterClicked {Element} - Clicked filter element
+	 * @param pointType {number} - Id of point type, 0 for all types
+	 */
+	function markPointTypeFiltersAsDisabled(filterClicked, pointType) {
+		var filters;
+
+		filterClicked.classList.remove('enabled');
+
+		if (pointType === 0) {
+			filters = pointTypeFiltersContainer.getElementsByClassName('point_type');
+
+			Array.prototype.forEach.call(filters, function (filterElement) {
+				filterElement.classList.remove('enabled');
+			});
+		} else {
+			document.getElementById('point_type_all').classList.remove('enabled');
+		}
+	}
+
+	/**
+	 * @desc Mark filters for given point type as enabled
+	 * @param filterClicked {Element} - Clicked filter element
+	 * @param pointType {number} - Id of point type, 0 for all types
+	 */
+	function markPointTypeFiltersAsEnabled(filterClicked, pointType) {
+		var filters;
+
+		if (pointType === 0) {
+			filters = pointTypeFiltersContainer.getElementsByClassName('point_type');
+
+			Array.prototype.forEach.call(filters, function (filterElement) {
+				filterElement.classList.add('enabled');
+			});
+		} else {
+			filterClicked.classList.add('enabled');
+
+			if (pointTypes.length === pointTypeFiltersContainer.getElementsByClassName('point_type enabled').length) {
+				document.getElementById('point_type_all').classList.add('enabled');
+			}
+		}
+	}
+
+	/**
+	 * @desc Add event listener on click event for point type filters
+	 */
+	function bindPointTypeFilters() {
+		pointTypeFiltersContainer.addEventListener('click', function (e) {
+			var filterClicked = e.target,
+				pointType,
+				points,
+				i;
+
+			if (filterClicked.tagName === 'UL') {
+				// we are too high in DOM, abort
+				return;
+			}
+
+			if (filterClicked.parentNode.tagName === 'LI') {
+				filterClicked = filterClicked.parentNode;
+			}
+
+			pointType = parseInt(filterClicked.getAttribute('data-point-type'), 10);
+
+			points = getPointsByType(pointType);
+
+			if (filterClicked.classList.contains('enabled')) {
+				markPointTypeFiltersAsDisabled(filterClicked, pointType);
+
+				// hide points
+				for (i = 0; i < points.length; i++) {
+					points[i].classList.add('hidden');
+				}
+			} else {
+				markPointTypeFiltersAsEnabled(filterClicked, pointType);
+
+				// show points
+				for (i = 0; i < points.length; i++) {
+					points[i].classList.remove('hidden');
+				}
+			}
+		}, false);
+	}
+
+	/**
+	 * @desc Create points and filters for them
+	 * @param config {object}
+	 */
+	function createPoints(config) {
+		pointTypes = config.types;
+
+		addAllPointsFilter();
+
+		config.types.forEach(function (pointType) {
+			addPointType(pointType);
+		});
+
+		config.points.forEach(function (point) {
+			addPointOnMap(point);
+		});
+
+		bindPointTypeFilters();
 	}
 
 	/**
@@ -120,11 +283,13 @@
 	}
 
 	/**
-	 * @desc Create new map and add points to it
+	 * @desc Create new map
 	 * @param config {object}
 	 */
 	function createMap(config) {
-		var defaultMinZoom = getMinZoomLevel(
+		var zoomControl, defaultMinZoom;
+
+		defaultMinZoom = getMinZoomLevel(
 			config.layer.maxZoom,
 			Math.max(config.width, config.height),
 			Math.max(
@@ -138,9 +303,10 @@
 		}
 
 		map = L.map(mapContainerId, {
-				minZoom: config.layer.minZoom,
-				maxZoom: config.layer.maxZoom
-			});
+			minZoom: config.layer.minZoom,
+			maxZoom: config.layer.maxZoom,
+			zoomControl: false
+		});
 		L.tileLayer(config.pathTemplate, config.layer).addTo(map);
 
 		if (config.hasOwnProperty('boundaries')) {
@@ -157,10 +323,15 @@
 			Math.max(config.zoom, defaultMinZoom)
 		);
 
-		config.points.forEach(function (point) {
-			addPointOnMap(point);
+		zoomControl = L.control.zoom({
+			position: 'bottomright'
 		});
+		map.addControl(zoomControl);
+
+		createPoints(config);
 	}
+
+	pointTypeFiltersContainer = document.getElementById(pointTypesContainerId);
 
 	createMap(window.mapSetup);
 
