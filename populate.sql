62f8b05a9125a7d41adb217d175d8879a0afbd91
aquilax <evgeniy@wikia-inc.com>
2014-06-18 12:20:08 +0200

    Updated populate script and DB schema

--- populate.sql
+++ populate.sql
@@ -9,7 +9,16 @@ height = 524288,
 min_zoom = 0,
 max_zoom = 524287,
 status = 0,
-created_on = CURRENT_TIMESTAMP,
+created_on = '2014-01-01 12:00:00',
 created_by = 'Wikia',
-attribution = 'TBD', -- TODO Add proper attribution here
+attribution = 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png"> | © OpenStreetMap contributors', -- Check MOB-1875 for the copy
 subdomains = '1234';
+
+
+INSERT INTO poi_category VALUES
+(1, 'Location', NULL, NULL, 0, 0, 1, 1, '2014-01-01 12:00:00', 'Wikia'),
+(2, 'Quest', NULL, NULL, 0, 0, 1, 2, '2014-01-01 12:00:00', 'Wikia'),
+(3, 'Character', NULL, NULL, 0, 0, 1, 3, '2014-01-01 12:00:00', 'Wikia'),
+(4, 'Item', NULL, NULL, 0, 0, 1, 4, '2014-01-01 12:00:00', 'Wikia'),
+(5, 'Event', NULL, NULL, 0, 0, 1, 5, '2014-01-01 12:00:00', 'Wikia'),
+(6, 'Other', NULL, NULL, 0, 0, 1, 6, '2014-01-01 12:00:00', 'Wikia');
