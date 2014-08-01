ffe05cf102e6143cbfdb414357f96a2cfb7e037c
nAndy <nandy@wikia-inc.com>
2014-08-01 20:28:27 +0200

    added canvas and underscore, extracted pixel data for 1px borders

--- tools/getBgColorForImage.js
+++ tools/getBgColorForImage.js
@@ -1,168 +1,68 @@
 'use strict';
 
-var gm = require('gm'),
-	Q = require('q'),
-	fs = require('fs'),
+var Canvas = require('canvas'),
+	gm = require('gm'),
+	_ = require('underscore'),
 
+	Image = Canvas.Image,
 	dir = __dirname + '/images/',
-	mapImages = ['millenium_falcoln.jpg', 'westeros.jpg', 'avatar.jpg', 'one_world.jpg'],
-	img = gm(dir + mapImages[3])
-		.options({imageMagick: true}),
-	imgData = {},
-	defaultCropSize = 50,
-	tempFiles = {
-		top: 'top.jpg',
-		right: 'right.jpg',
-		bottom: 'bottom.jpg',
-		left: 'left.jpg',
-		combined: 'combined.jpg'
-	};
+	mapImages = ['test.jpg', 'millenium_falcon.jpg', 'westeros.jpg', 'avatar.jpg', 'one_world.jpg'],
+	mapImage = gm(dir + mapImages[0]).options({imageMagick: true}),
+	canvas,
+	ctx,
+	img,
+	pixels = new Array();
 
-function identifyImage(image) {
-	var deferred = Q.defer();
+function pixelArrayToRgbArray(pixels) {
+	var result = [],
+		numPixels = pixels.data.length / 4;
 
-	image.identify(function (err, results) {
-		if (err) {
-			deferred.reject(err);
-			return;
-		}
-
-		deferred.resolve(results);
-	});
-
-	return deferred.promise;
-}
-
-function crop(width, height, x, y, newImgName) {
-	var deferred = Q.defer();
-
-	img
-		.crop(width, height, x, y)
-		.write(dir + newImgName, function (err) {
-			if (err) {
-				deferred.reject(err);
-				return;
-			}
-
-			// console.log(this.outname + " created :: " + arguments[3]);
-			deferred.resolve();
+	for (var i = 0; i < numPixels; i++) {
+		result.push({
+			'red': pixels.data[i*4],
+			'green': pixels.data[i*4+1],
+			'blue': pixels.data[i*4+2],
+			'alpha': pixels.data[i*4+3]
 		});
-
-	return deferred.promise;
-}
-
-function rotate() {
-	var deferred = Q.defer();
-
-	console.log('Rotating...');
-
-	gm(dir + tempFiles.right)
-		.options({imageMagick: true})
-		.rotate('#f00', 90)
-		.write(dir + tempFiles.right, function (err) {
-			if (err) {
-				console.log(arguments);
-				deferred.reject(err);
-			}
-
-			// console.log(this.outname + " created  ::  " + arguments[3]);
-		});
-
-	gm(dir + tempFiles.left)
-		.options({imageMagick: true})
-		.rotate('#f00', 90)
-		.write(dir + tempFiles.left, function (err) {
-			if (err) {
-				console.log(arguments);
-				deferred.reject(err);
-			}
-
-			// console.log(this.outname + " created  ::  " + arguments[3]);
-			deferred.resolve();
-		});
-
-	return deferred.promise;
-}
-
-function combine() {
-	var deferred = Q.defer();
-
-	console.log('Combining...');
-
-	gm(dir + tempFiles.top)
-		.options({imageMagick: true})
-		.append(dir + tempFiles.right, dir + tempFiles.bottom, dir + tempFiles.left)
-		.write(dir + tempFiles.combined, function (err) {
-			if (err) {
-				console.log(err);
-				deferred.reject(err);
-			} else {
-				// console.log(this.outname + " created  ::  " + arguments[3]);
-				require('child_process').exec('open ' + dir + tempFiles.combined);
-				deferred.resolve();
-			}
-		});
-
-	return deferred.promise;
-}
-
-function handleError(err) {
-	if (err) {
-		console.log(err);
 	}
+
+	return result;
 }
 
-identifyImage(img).then(function (results) {
-	var rightCropX = parseInt(imgData.width - defaultCropSize, 10),
-		bottomCropX = parseInt(imgData.height - defaultCropSize, 10);
+mapImage.size(function (err,res) {
+	var pixelData;
+	console.log('Getting the size...');
 
-	console.log('Getting image data...');
+	if (err) {
+		throw err;
+	}
 
-	imgData.width = results.size.width;
-	imgData.height = results.size.height;
-	imgData.channelStats = results['Channel statistics'];
+	console.log('Creating image and canvas with it...');
+	img = new Image;
+	img.src = dir + mapImages[0];
 
-	console.log('Cropping...');
+	canvas = new Canvas(res.width, res.height);
+	ctx = canvas.getContext('2d');
+	ctx.drawImage(img, 0, 0, img.width, img.height);
 
-	crop(imgData.width, defaultCropSize, 0, 0, tempFiles.top).then(function () {
-			crop(defaultCropSize, imgData.height, rightCropX, 0, tempFiles.right).then(function () {
-					crop(imgData.width, defaultCropSize, bottomCropX, 0, tempFiles.bottom).then(function () {
-							crop(defaultCropSize, imgData.height, 0, 0, tempFiles.left).then(function () {
-									rotate().then(function () {
-											combine().then(function () {
-												var combinedImg = gm(
-													dir + tempFiles.combined
-												).options({imageMagick: true});
+	console.log('Getting pixels data...');
 
-												identifyImage(combinedImg).then(function (results) {
-													var combinedImgData = [];
+	// top border
+	pixelData = ctx.getImageData(0, 0, img.width - 1, 1);
+	pixels.push(pixelArrayToRgbArray(pixelData));
 
-													combinedImgData.width = results.size.width;
-													combinedImgData.height = results.size.height;
-													combinedImgData.channelStats = results['Channel statistics'];
+	// right border
+	pixelData = ctx.getImageData(img.width - 1, 0, 1, img.height - 1);
+	pixels.push(pixelArrayToRgbArray(pixelData));
 
-													console.log('Original image mean color:');
-													console.log('R: ' + imgData.channelStats.Red.mean);
-													console.log('G: ' + imgData.channelStats.Green.mean);
-													console.log('B: ' + imgData.channelStats.Blue.mean);
+	// bottom border
+	pixelData = ctx.getImageData(0, img.height - 1, img.width, 1);
+	pixels.push(pixelArrayToRgbArray(pixelData));
 
-													console.log('Combined image:');
-													console.log('R: ' + combinedImgData.channelStats.Red.mean);
-													console.log('G: ' + combinedImgData.channelStats.Green.mean);
-													console.log('B: ' + combinedImgData.channelStats.Blue.mean);
+	// left border
+	pixelData = ctx.getImageData(0, 0, 1, img.height - 1);
+	pixels.push(pixelArrayToRgbArray(pixelData));
 
-													console.log('Removing temporary files...');
-													for (var fileName in tempFiles) {
-														if (tempFiles.hasOwnProperty(fileName)) {
-															fs.unlink(dir + tempFiles[fileName], handleError);
-														}
-													}
-													console.log('Done!');
-												});
-											});
-									});
-							});
-					});
-			});
-	});
+	console.log(_.flatten(pixels));
+	console.log('Done!');
 });
