2fa243f2ee397056dc82a191db1baf5107943edd
nAndy <nandy@wikia-inc.com>
2014-08-04 18:50:47 +0200

    removed node-canvas module and use get-pixels instead

--- tools/getBgColorForImage.js
+++ tools/getBgColorForImage.js
@@ -1,83 +1,65 @@
 'use strict';
 
-var fs = require('fs'),
-	Canvas = require('canvas'),
-	_ = require('underscore'),
+var getPixels = require('get-pixels'),
+	underscore = require('underscore'),
 
-	Image = Canvas.Image,
 	dir = __dirname + '/images/',
 	mapImages = ['test.png', 'millenium_falcon.jpg', 'westeros.jpg', 'avatar.jpg', 'one_world.jpg'],
-	mapImageName = mapImages[4],
-	canvas,
-	ctx,
-	img,
-	imgWidth,
-	imgHeight,
-	pixels = [],
-	pixelData,
-	results;
+	mapImageName = mapImages[0];
 
-function pixelArrayToRgbArray(pixels) {
-	var result = [],
-		numPixels = pixels.data.length / 4;
-
-	for (var i = 0; i < numPixels; i++) {
-		result.push({
-			'red': pixels.data[i*4],
-			'green': pixels.data[i*4+1],
-			'blue': pixels.data[i*4+2],
-			'alpha': pixels.data[i*4+3]
-		});
-	}
-
-	return result;
+function getColorObject(x, y, pixels) {
+	return {
+		'red': pixels.get(y, x, 0),
+		'green': pixels.get(y, x, 1),
+		'blue': pixels.get(y, x, 2),
+		'alpha': pixels.get(y, x, 3)
+	};
 }
 
-fs.readFile(dir + mapImageName, function (err, image) {
+console.log('Getting image pixels for ' + mapImageName + '...');
+getPixels(dir + mapImageName, function (err, pixels) {
 	if (err) {
 		throw err;
 	}
 
-	console.log('Creating image and canvas with it...');
-	img = new Image;
-	img.src = dir + mapImageName;
-	imgWidth = img.width;
-	imgHeight = img.height;
-
-	canvas = new Canvas(imgWidth, imgHeight);
-	ctx = canvas.getContext('2d');
-	ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
-
-	console.log('Getting pixels data...');
+	var dimensions = pixels.shape.slice(),
+		imgWidth = dimensions[1],
+		imgHeight = dimensions[0],
+		i,
+		results = [],
+		sortedResults = [];
 
 	// top border
-	pixelData = ctx.getImageData(0, 0, imgWidth, 1);
-	pixels.push(pixelArrayToRgbArray(pixelData));
-
-	// right border
-	pixelData = ctx.getImageData(imgWidth - 1, 0, 1, imgHeight);
-	pixels.push(pixelArrayToRgbArray(pixelData));
-
-	// bottom border
-	pixelData = ctx.getImageData(0, imgHeight - 1, imgWidth, 1);
-	pixels.push(pixelArrayToRgbArray(pixelData));
+	for (i = 0; i < imgWidth; i++) {
+		results.push(getColorObject(i, 0, pixels));
+	}
 
 	// left border
-	pixelData = ctx.getImageData(0, 0, 1, imgHeight);
-	pixels.push(pixelArrayToRgbArray(pixelData));
+	for (i = 0; i < imgHeight; i++) {
+		results.push(getColorObject(49, i, pixels));
+	}
 
-	results = _.countBy(_.flatten(pixels), function (pixel) {
+	// bottom border
+	for (i = 0; i < imgWidth; i++) {
+		results.push(getColorObject(i, 49, pixels));
+	}
+
+	// right border
+	for (i = 0; i < imgWidth; i++) {
+		results.push(getColorObject(0, i, pixels));
+	}
+
+	results = underscore.countBy(underscore.flatten(results), function (pixel) {
 		return 'rgba(' + pixel.red + ',' + pixel.green + ',' + pixel.blue + ',' + pixel.alpha + ')';
 	});
 
-	var sortedResults = [];
-	_.each(results, function (key, value) {
+	underscore.each(results, function (key, value) {
 		sortedResults.push({
 			color: value,
 			count: key
-		})
+		});
 	});
 
-	results = _.sortBy(sortedResults, 'count');
-	console.log('The most used color is: ' + results[results.length-1].color);
+	results = underscore.sortBy(sortedResults, 'count');
+	console.log('The most used color is: ' + results[results.length - 1].color);
 });
