94df0d7ebd29f7c2ce63458c57e0b1dd7852bb2f
nAndy <nandy@wikia-inc.com>
2014-08-04 16:49:49 +0200

    removed gm module, did small refactoring

--- tools/getBgColorForImage.js
+++ tools/getBgColorForImage.js
@@ -1,18 +1,21 @@
 'use strict';
 
-var Canvas = require('canvas'),
-	gm = require('gm'),
+var fs = require('fs'),
+	Canvas = require('canvas'),
 	_ = require('underscore'),
 
 	Image = Canvas.Image,
 	dir = __dirname + '/images/',
 	mapImages = ['test.png', 'millenium_falcon.jpg', 'westeros.jpg', 'avatar.jpg', 'one_world.jpg'],
-	mapImageName = mapImages[0],
-	mapImage = gm(dir + mapImageName).options({imageMagick: true}),
+	mapImageName = mapImages[4],
 	canvas,
 	ctx,
 	img,
-	pixels = [];
+	imgWidth,
+	imgHeight,
+	pixels = [],
+	pixelData,
+	results;
 
 function pixelArrayToRgbArray(pixels) {
 	var result = [],
@@ -30,12 +33,7 @@ function pixelArrayToRgbArray(pixels) {
 	return result;
 }
 
-mapImage.size(function (err,res) {
-	var pixelData,
-		results;
-
-	console.log('Getting the size of ' + mapImageName + '...');
-
+fs.readFile(dir + mapImageName, function (err, image) {
 	if (err) {
 		throw err;
 	}
@@ -43,27 +41,29 @@ mapImage.size(function (err,res) {
 	console.log('Creating image and canvas with it...');
 	img = new Image;
 	img.src = dir + mapImageName;
+	imgWidth = img.width;
+	imgHeight = img.height;
 
-	canvas = new Canvas(res.width, res.height);
+	canvas = new Canvas(imgWidth, imgHeight);
 	ctx = canvas.getContext('2d');
-	ctx.drawImage(img, 0, 0, img.width, img.height);
+	ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
 
 	console.log('Getting pixels data...');
 
 	// top border
-	pixelData = ctx.getImageData(0, 0, img.width, 1);
+	pixelData = ctx.getImageData(0, 0, imgWidth, 1);
 	pixels.push(pixelArrayToRgbArray(pixelData));
 
 	// right border
-	pixelData = ctx.getImageData(img.width - 1, 0, 1, img.height);
+	pixelData = ctx.getImageData(imgWidth - 1, 0, 1, imgHeight);
 	pixels.push(pixelArrayToRgbArray(pixelData));
 
 	// bottom border
-	pixelData = ctx.getImageData(0, img.height - 1, img.width, 1);
+	pixelData = ctx.getImageData(0, imgHeight - 1, imgWidth, 1);
 	pixels.push(pixelArrayToRgbArray(pixelData));
 
 	// left border
-	pixelData = ctx.getImageData(0, 0, 1, img.height);
+	pixelData = ctx.getImageData(0, 0, 1, imgHeight);
 	pixels.push(pixelArrayToRgbArray(pixelData));
 
 	results = _.countBy(_.flatten(pixels), function (pixel) {
