75ec480e82333bdaf47451fa2c0c09e4fe151b5a
Rafal Leszczynski <rafal@wikia-inc.com>
2014-08-06 10:39:52 +0200

    add AMD module for config params

---
+++ assets/scripts/im.config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+define('im.config', ['im.window'], function(w) {
+	return {
+		mapConfig: w.mapSetup,
+		messages: w.mapSetup.i18n,
+		pontoTimeout: 500,
+		popupWidthWithPhoto: 414,
+		popupWidthWithoutPhoto: 314,
+		mobileMaxWidth: 430,
+		mobilePopupWidth: 310,
+		autoZoomPadding: 0.01,
+		poiIconWidth: 30,
+		poiIconHeight: 30,
+		photoWidth: 90,
+		photoHeight: 90,
+		mapContainerId: 'map',
+		pointTypeFiltersContainerId: 'pointTypes',
+		editPointTypesButtonId: 'editPointTypes',
+		allPointTypesFilterId: 'allPointTypes',
+		pontoBridgeModule: 'wikia.intMap.pontoBridge',
+		uiControlsPosition: 'bottomright'
+	};
+});
