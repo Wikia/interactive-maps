018ff70ab5d759343ea1bab675cf3a8eac6b6f43
nAndy <nandy@wikia-inc.com>
2014-08-13 16:58:53 +0200

    first version of script updating tilesets background

---
+++ tools/updateTilesetBackground.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+'use strict';
+
+/**
+ * This is part of MWEB-869
+ *
+ * The script loops through all tile sets with status OK,
+ * calculates their background color by using lib/imageBackground.js
+ * and updates the field in tile_set table in DB
+ */
+
+var dbCon = require('../lib/db_connector'),
+	config = require('../lib/config'),
+	utils = require('../lib/utils'),
+	imageBackground = require('../lib/imageBackground'),
+	tmpDir = config.tmp,
+	tableName = 'tile_set';
+
+/**
+ * @desc Updates a tiles' set row in database
+ * @param {object} data
+ * @param {object} conn
+ * @returns {object}
+ */
+function updateBackgroundColor(data, conn) {
+	console.log('Updating background color for tiles\' set with id #' + data.id + '...');
+	console.log('Setting its color to: ' + data.bgColor + '...');
+
+	return dbCon.update(
+		conn,
+		tableName,
+		{
+			background_color: data.bgColor
+		},
+		{
+			id: data.id
+		}
+	);
+}
+
+dbCon.getConnection(dbCon.connType.master)
+	.then(function (conn) {
+		dbCon.select(conn, tableName, [
+			'id',
+			'image'
+		], {
+			'status': utils.tileSetStatus.ok,
+			'background_color': '#ddd'
+		}).then(function (data) {
+			console.log('Found ' + data.length + ' tiles\' sets...');
+
+			data.forEach(function (row) {
+				imageBackground.getBgColorForImage({
+					id: row.id,
+					image: row.image,
+					dir: tmpDir
+				})
+				.then(function (data) {
+					return updateBackgroundColor(data, conn);
+				})
+				.catch(function (err) {
+					console.log(err);
+				});
+			});
+		});
+	})
+	.catch(function (err) {
+		console.log(err);
+	});
