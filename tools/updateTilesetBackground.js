89acc42c26535d8cf957ccaa3afdefcec47e1d18
nAndy <nandy@wikia-inc.com>
2014-08-14 12:39:12 +0200

    refactoring: because of strange results cause possibly by race condition made the script work in sequential way

--- tools/updateTilesetBackground.js
+++ tools/updateTilesetBackground.js
@@ -6,15 +6,20 @@
  *
  * The script loops through all tile sets with status OK,
  * calculates their background color by using lib/imageBackground.js
- * and updates the field in tile_set table in DB
+ * and updates the field in tile_set table in DB.
+ *
+ * We did have to make it sequential because of race conditions
+ * among modules (fetchImage, imageBackground).
  */
 
 var dbCon = require('../lib/db_connector'),
 	config = require('../lib/config'),
 	utils = require('../lib/utils'),
+	fetchImage = require('../lib/fetchImage'),
 	imageBackground = require('../lib/imageBackground'),
 	tmpDir = config.tmp,
-	tableName = 'tile_set';
+	tableName = 'tile_set',
+	tileSets = [];
 
 /**
  * @desc Updates a tiles' set row in database
@@ -38,6 +43,61 @@ function updateBackgroundColor(data, conn) {
 	);
 }
 
+/**
+ * @desc Helper function to call sequentially next tiles' set update
+ * @param {object} conn connection object created by db_connector.getConnection()
+ */
+function next(conn) {
+	if (tileSets.length > 0) {
+		updateTileset(tileSets.pop(), conn);
+	} else {
+		console.log('Done.');
+		process.exit();
+	}
+}
+
+/**
+ * @desc Downloads the image, calculates bg color, updates DB and calls itself with different tileset's data
+ * @param {object} row object with data from database
+ * @param {object} conn connection object created by db_connector.getConnection()
+ */
+function updateTileset(row, conn) {
+	var tileSetId = row.id,
+		imageName = row.image,
+		bucketName = utils.getBucketName(
+			config.bucketPrefix + config.tileSetPrefix,
+			tileSetId
+		),
+		imageUrl = utils.imageUrl(
+			config.dfsHost,
+			bucketName,
+			imageName
+		),
+		data = {
+			id: tileSetId,
+			image: imageName,
+			fileUrl: imageUrl,
+			bucketName: bucketName,
+			dir: tmpDir
+		};
+
+	fetchImage(data)
+		.then(imageBackground.getBgColorForImage)
+		.then(function (data) {
+			return updateBackgroundColor(data, conn);
+		})
+		.then(function () {
+			next(conn);
+		})
+		.catch(function (err) {
+			console.log(err);
+			next(conn);
+		});
+}
+
+/**
+ * Main part of the script
+ */
 dbCon.getConnection(dbCon.connType.master)
 	.then(function (conn) {
 		dbCon.select(conn, tableName, [
@@ -49,29 +109,8 @@ dbCon.getConnection(dbCon.connType.master)
 		}).then(function (data) {
 			var found = data.length;
 			console.log('Found ' + found + ' tiles\' sets...');
-
-			data.forEach(function (row) {
-				imageBackground.getBgColorForImage({
-					id: row.id,
-					image: row.image,
-					dir: tmpDir
-				})
-				.then(function (data) {
-					found--;
-					return updateBackgroundColor(data, conn);
-				}).then(function () {
-					if (found === 1) {
-						process.exit();
-					}
-				})
-				.catch(function (err) {
-					found--;
-					console.log(err);
-					if (found === 0) {
-						process.exit();
-					}
-				});
-			});
+			tileSets = data;
+			next(conn);
 		});
 	})
 	.catch(function (err) {
