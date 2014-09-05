d309caf4af94d70f8f8a2423641ac0a672bb3716
Rafal Leszczynski <rafal@wikia-inc.com>
2014-09-05 14:54:20 +0200

    move api helper methods to separate module

--- assets/scripts/im.pontoCommunicationAPI.js
+++ assets/scripts/im.pontoCommunicationAPI.js
@@ -2,8 +2,8 @@
 
 define(
 	'im.pontoCommunicationAPI',
-	['ponto', 'im.config', 'im.leafletWrapper', 'im.poi', 'im.map'],
-	function (ponto, config, L, poiModule, mapModule) {
+	['ponto', 'im.config', 'im.leafletWrapper', 'im.pontoCommunicationAPI.utils', 'im.map'],
+	function (ponto, config, L, apiUtils, mapModule) {
 		var apiConfig = config.pontoCommunicationAPI,
 			playerMarker,
 			map;
@@ -27,35 +27,39 @@ define(
 			 *     }
 			 * @param {number} callbackId - ponto callback ID
 			 */
-			this.setPlayerCurrentLocation = function(params, callbackId) {
+			this.setPlayerCurrentLocation = function (params, callbackId) {
 				var responseMessage = apiConfig.responseMessages.setPlayerLocation,
-					validationResult = validateParams(params),
+					mapBoundaries = mapModule.getMapBoundaries(),
+					validationResult = apiUtils.validateParams(params, mapBoundaries),
 					responseCode = apiConfig.responseCodes.success,
+					responseContent = {},
 					latLng;
 
 				if (validationResult.success) {
 					latLng = L.latLng(params.lat, params.lng);
 
 					if (!map.hasLayer(playerMarker)) {
-						createPlayerMarker({
+						playerMarker = apiUtils.createPlayerMarker({
 							lat: params.lat,
 							lon: params.lng
 						});
+						playerMarker.addTo(map);
 					} else {
-						updatePlayerMarkerLocation(latLng);
+						apiUtils.updatePlayerMarkerLocation(playerMarker, latLng);
 					}
 
 					if (params.centerMap) {
-						centerMapToPlayerLocation(latLng, params.zoom);
+						apiUtils.centerMapToPlayerLocation(map, latLng, params.zoom);
 					}
 				} else {
 					responseMessage = validationResult.message;
 					responseCode = apiConfig.responseCodes.invalidParams;
+					responseContent.boundaries = mapBoundaries;
 				}
 
 				ponto.respond(
-					createPontoResponse(
-						validationResult.success, responseCode, responseMessage, mapModule.getMapBoundaries()
+					apiUtils.createPontoResponse(
+						validationResult.success, responseCode, responseMessage, responseContent
 					), callbackId);
 			};
 
@@ -64,117 +68,21 @@ define(
 			 * @param {object} params - params sent via ponto
 			 * @param {number} callbackId - ponto callback ID
 			 */
-			this.removePlayerLocation = function(params, callbackId) {
+			this.removePlayerLocation = function (params, callbackId) {
 				var responseMessage = apiConfig.responseMessages.removePlayerLocation;
 
 				map.removeLayer(playerMarker);
 
-				ponto.respond(createPontoResponse(true, apiConfig.responseCodes.success, responseMessage), callbackId);
+				ponto.respond(
+					apiUtils.createPontoResponse(true, apiConfig.responseCodes.success, responseMessage),
+					callbackId
+				);
 			};
 		}
-
-		// HELPER METHODS *****************************************************************
-
-		/**
-		 * @desc creates ponto response object
-		 * @param {Boolean} success
-		 * @param {Number} responseCode
-		 * @param {String} message - response message
-		 * @param {Object=} content - additional data
-		 * @returns {Object}
-		 */
-		function createPontoResponse(success, responseCode, message, content) {
-			return {
-				success: success,
-				responseCode: responseCode,
-				message: message,
-				content: content || {}
-			};
-		}
-
-		/**
-		 * @desc helper function that validates player location params
-		 * @param {Object} params - params sent via ponto
-		 */
-		function validateParams(params) {
-			var mapBoundaries = mapModule.getMapBoundaries(),
-				result = {
-					success: false
-				};
-
-			if (!validateTypes(params)) {
-				result.message = apiConfig.responseMessages.invalidParamTypes;
-			} else if (mapBoundaries && !isPlayerLocationInMapBounds(mapBoundaries, params.lat, params.lng)) {
-				result.message = apiConfig.responseMessages.outOfMapBounds;
-			} else {
-				result.success = true;
-			}
-
-			return result;
-		}
-
-		/**
-		 * @desc validates params types
-		 * @param {Object} params - params sent via ponto
-		 * @returns {boolean}
-		 */
-		function validateTypes(params) {
-			return typeof params.lat === 'number' && typeof params.lng === 'number';
-		}
-
-		/**
-		 * @desc checks if player location fits in map boundaries
-		 * @param {Object} bounds - map boundaries
-		 * @param {Number} lat - latitude
-		 * @param {Number} lng - longitude
-		 * @returns {boolean}
-		 */
-		function isPlayerLocationInMapBounds(bounds, lat, lng) {
-			return lat <= bounds.north &&
-				lat >= bounds.south &&
-				lng >= bounds.west &&
-				lng <= bounds.east;
-		}
-
-		/**
-		 * @desc helper function that creates marker for player location and adds it to map
-		 * @param {object} params
-		 */
-		function createPlayerMarker(params) {
-			var markerSize = apiConfig.defaultMarkerSize;
-
-			playerMarker = poiModule.createPoiMarker(
-				params,
-				L.icon({
-					iconUrl: apiConfig.defaultPlayerIcon,
-					iconSize: [markerSize, markerSize]
-				})
-			);
-			playerMarker.addTo(map);
-		}
-
-		/**
-		 * @desc helper function that updates player marker location
-		 * @param {object} latLng - latLng Leaflet object
-		 */
-		function updatePlayerMarkerLocation(latLng) {
-			playerMarker.setLatLng(latLng);
-		}
-
-		/**
-		 * @desc helper function that centers map and sets its zoom level to player current location
-		 * @param {object} latLng - latLng Leaflet object
-		 * @param {number} zoom - map zoom level
-		 */
-		function centerMapToPlayerLocation(latLng, zoom) {
-			map.setView(latLng, zoom);
-		}
-
-		// END OF HELPER METHODS *******************************************************************
 
 		// PontoBaseHandler extension pattern - check Ponto documentation for details
 		ponto.PontoBaseHandler.derive(PontoCommunicationAPI);
-		PontoCommunicationAPI.getInstance = function() {
+		PontoCommunicationAPI.getInstance = function () {
 			return new PontoCommunicationAPI();
 		};
 
---
+++ assets/scripts/im.pontoCommunicationAPI.utils.js
@@ -0,0 +1,113 @@
+define(
+	'im.pontoCommunicationAPI.utils',
+	['im.leafletWrapper', 'im.config', 'im.poi'],
+	function (L, config, poiModule) {
+		'use strict';
+
+		var apiConfig = config.pontoCommunicationAPI;
+
+		/**
+		 * @desc creates ponto response object
+		 * @param {Boolean} success
+		 * @param {Number} responseCode
+		 * @param {String} message - response message
+		 * @param {Object=} content - additional data
+		 * @returns {Object}
+		 */
+		function createPontoResponse(success, responseCode, message, content) {
+			return {
+				success: success,
+				responseCode: responseCode,
+				message: message,
+				content: content || {}
+			};
+		}
+
+		/**
+		 * @desc helper function that validates player location params
+		 * @param {Object} params - params sent via ponto
+		 * @param {Object} mapBoundaries -  map boundaries
+		 */
+		function validateParams(params, mapBoundaries) {
+			var result = {
+					success: false
+				};
+
+			if (!validateTypes(params)) {
+				result.message = apiConfig.responseMessages.invalidParamTypes;
+			} else if (mapBoundaries && !isPlayerLocationInMapBounds(mapBoundaries, params.lat, params.lng)) {
+				result.message = apiConfig.responseMessages.outOfMapBounds;
+			} else {
+				result.success = true;
+			}
+
+			return result;
+		}
+
+		/**
+		 * @desc validates params types
+		 * @param {Object} params - params sent via ponto
+		 * @returns {boolean}
+		 */
+		function validateTypes(params) {
+			return typeof params.lat === 'number' && typeof params.lng === 'number';
+		}
+
+		/**
+		 * @desc checks if player location fits in map boundaries
+		 * @param {Object} bounds - map boundaries
+		 * @param {Number} lat - latitude
+		 * @param {Number} lng - longitude
+		 * @returns {boolean}
+		 */
+		function isPlayerLocationInMapBounds(bounds, lat, lng) {
+			return lat <= bounds.north &&
+				lat >= bounds.south &&
+				lng >= bounds.west &&
+				lng <= bounds.east;
+		}
+
+		/**
+		 * @desc helper function that creates marker for player location and adds it to map
+		 * @param {Object} params
+		 */
+		function createPlayerMarker(params) {
+			var markerSize = apiConfig.defaultMarkerSize;
+
+			return poiModule.createPoiMarker(
+				params,
+				L.icon({
+					iconUrl: apiConfig.defaultPlayerIcon,
+					iconSize: [markerSize, markerSize]
+				})
+			);
+		}
+
+		/**
+		 * @desc helper function that updates player marker location
+		 * @param {Object} playerMarker - leaflet marker object
+		 * @param {Object} latLng - latLng Leaflet object
+		 */
+		function updatePlayerMarkerLocation(playerMarker, latLng) {
+			playerMarker.setLatLng(latLng);
+		}
+
+		/**
+		 * @desc helper function that centers map and sets its zoom level to player current location
+		 * @param {Object} map - leaflet map object
+		 * @param {Object} latLng - latLng Leaflet object
+		 * @param {Number} zoom - map zoom level
+		 */
+		function centerMapToPlayerLocation(map, latLng, zoom) {
+			map.setView(latLng, zoom);
+		}
+
+		return {
+			createPontoResponse: createPontoResponse,
+			validateParams: validateParams,
+			createPlayerMarker: createPlayerMarker,
+			updatePlayerMarkerLocation: updatePlayerMarkerLocation,
+			centerMapToPlayerLocation: centerMapToPlayerLocation
+		};
+	}
+);
