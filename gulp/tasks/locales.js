bca42ee38dcdd036870268faab87eee07d7766cc
nAndy <nandy@wikia-inc.com>
2014-09-05 20:23:28 +0200

    CR changes: refactored locales gulp task

--- gulp/tasks/locales.js
+++ gulp/tasks/locales.js
@@ -1,26 +1,33 @@
 'use strict';
 
 var gulp = require('gulp'),
-	exec = require('child_process').exec,
+	http = require('http'),
 	Q = require('q'),
+	fs = require('fs'),
 	config = require('../../lib/config.js'),
-	localesDir = '../../locales/',
+	localesDir = './locales/',
 	translationFile = localesDir + 'translations.json';
 
 gulp.task('locales', function () {
 	console.assert(typeof config.translationUrl === 'string', 'Translation URL not set');
-	var cmd = 'curl "' + config.translationUrl + '" -o ' + translationFile;
-	return exec(cmd, function () {
-		// check if the downloaded translation is consistent
-		var translation = require(translationFile),
-			deferred = Q.defer();
 
-		if (typeof translation.messages === 'object') {
-			deferred.reject('Translation is broken');
-		} else {
-			deferred.resolve('Translation updated');
+	var deferred = Q.defer(),
+		translationsData = '';
+
+	http.get(config.translationUrl, function (res) {
+		if (res.statusCode !== 200) {
+			deferred.reject('Unable to download translations');
 		}
 
-		return deferred.promise;
+		res.on('data', function (chunk) {
+			translationsData += chunk;
+		});
+
+		res.on('end', function () {
+			fs.writeFileSync(translationFile, translationsData);
+			deferred.resolve();
+		});
 	});
+
+	return deferred.promise;
 });
