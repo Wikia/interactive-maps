4c1674be09253cf1296f73bb6617fa656cfd570b
Rafal Leszczynski <rafal@wikia-inc.com>
2014-09-08 14:33:32 +0200

    clean up gulp file after refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,3 @@
 'use strict';
 
 require('./gulp');
-
-var gulp = require('gulp'),
-	nodemon = require('gulp-nodemon');
-
-gulp.task('dev', function () {
-	nodemon({
-		script: 'app.js',
-		ext: 'js',
-		ignore: ['tmp/**', 'node_modules/**'],
-		env: {
-			'NODE_ENV': 'devbox' //'production'
-		}
-	});
-
-	//here at least it is not restarted on file changes
-	//and this is not strictly needed for development
-	require('./kueServer');
-});
