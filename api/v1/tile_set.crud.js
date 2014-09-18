90bc0c12bfa50a96523532de428dd650b99b147e
Rafal Leszczynski <rafal@wikia-inc.com>
2014-09-18 09:36:04 +0200

    refactor get collection method

--- api/v1/tile_set.crud.js
+++ api/v1/tile_set.crud.js
@@ -45,6 +45,153 @@ var dbCon = require('./../../lib/db_connector'),
 	minSearchCharacters = 2;
 
 /**
+ * @desc creates tileSet image URL and returns it
+ * @param {Number} tileSetId
+ * @param {String} imageName
+ * @returns {String} - tileSet image URL
+ */
+function createTileSetImageUrl(tileSetId, imageName) {
+	return utils.imageUrl(
+		config.dfsHost,
+		utils.getBucketName(
+			config.bucketPrefix + config.tileSetPrefix,
+			tileSetId
+		),
+		imageName
+	);
+}
+
+/**
+ * @desc creates API URL for tileSetObject and returns it
+ * @param {Number} tileSetId
+ * @param {Object} req - HTTP request object
+ * @returns {String} - tileSet Object API URL
+ */
+function createTileSetAPIUrl(tileSetId, req) {
+	return utils.responseUrl(req, utils.addTrailingSlash(req.route.path), tileSetId);
+}
+
+/**
+ * @desc extends params of tileSet object
+ * @param {Object} tileSet
+ * @param {Object} req - HTTP request object
+ */
+function extendTileSetObject(tileSet, req) {
+	tileSet.image = createTileSetImageUrl(tileSet.id, tileSet.image);
+	tileSet.url = createTileSetAPIUrl(tileSet.id, req);
+}
+
+/**
+ * @desc processes collection of tileSets
+ * @param {Array} collection - collection of tileSets
+ * @param {Object} req - HTTP request object
+ * @param {Function} processFun - function for processing each element form the collection
+ * @returns {Array}
+ */
+function processTileSetCollection(collection, req, processFun) {
+	var length = collection.length,
+		i;
+
+	for (i = 0; i < length; i++) {
+		processFun(collection[i], req);
+	}
+
+	return collection;
+}
+
+/**
+ * @desc adds pagination to DB query
+ * @param {Object} query - Knex DB query object
+ * @param {Number} limit - number of results
+ * @param {Number} offset - offset from top
+ */
+function addPaginationToQuery(query, limit, offset) {
+	query
+		.limit(limit)
+		.offset(offset);
+}
+
+/**
+ * @desc adds search term to query
+ * @param {Object} query - Knex DB query object
+ * @param {String} search - search term
+ */
+function addSearchToQuery(query, search) {
+	query
+		.join('tile_set_search', 'tile_set.id', '=', 'tile_set_search.id')
+		.whereRaw('MATCH (tile_set_search.name) AGAINST (?)', [search])
+		.orderBy('created_on', 'desc');
+}
+
+/**
+ * @desc validates search term
+ * @param {String} search - search term
+ * @returns {Boolean}
+ */
+function validateSearchTerm(search) {
+	return search.length >= minSearchCharacters;
+}
+
+/**
+ * @desc CRUD function for getting collection of tileSets
+ * @param {Object} req - HTTP request object
+ * @param {Object} res - HTTP response object
+ * @param {Function} next
+ */
+function getTileSets(req, res, next) {
+	var dbConnType = dbCon.connType.all,
+		dbColumns = [
+			'tile_set.id',
+			'tile_set.name',
+			'tile_set.type',
+			'tile_set.status',
+			'tile_set.width',
+			'tile_set.height',
+			'tile_set.image'
+		],
+		filter = {
+			status: utils.tileSetStatus.ok
+		},
+		limit = parseInt(req.query.limit, 10) || false,
+		offset = parseInt(req.query.offset, 10) || 0,
+		search = req.query.search.trim() || false,
+		query = dbCon.knex(dbTable).column(dbColumns).where(filter);
+
+	// add search term to DB query
+	if (search) {
+		if (!validateSearchTerm(search)) {
+			next(errorHandler.badRequestError([
+				'Search string should be at least ' + minSearchCharacters + ' long.'
+			]));
+		}
+
+		limit = limit ? Math.min(searchLimit, limit) : searchLimit;
+		// TODO: do we need it?
+		//offset = 0;
+
+		addSearchToQuery(query, search);
+	}
+
+	// add pagination to DB query
+	if (limit) {
+		addPaginationToQuery(query, limit, offset);
+	}
+
+	// make a DB query
+	dbCon.getConnection(dbConnType, function (conn) {
+		query
+			.connection(conn)
+			.select()
+			.then(function (collection) {
+				res
+					.status(200)
+					.send(processTileSetCollection(collection, req, extendTileSetObject))
+					.end();
+			}, next);
+	}, next);
+}
+
+/**
  * @desc Creates CRUD collection based on configuration object passed as parameter
  * @returns {object} - CRUD collection
  */
@@ -52,64 +199,7 @@ var dbCon = require('./../../lib/db_connector'),
 module.exports = function createCRUD() {
 	return {
 		handler: {
-			GET: function (req, res, next) {
-				var dbColumns = [
-						'tile_set.id',
-						'tile_set.name',
-						'tile_set.type',
-						'tile_set.status',
-						'tile_set.width',
-						'tile_set.height',
-						'tile_set.image'
-					],
-					filter = {
-						status: utils.tileSetStatus.ok
-					},
-					limit = parseInt(req.query.limit, 10) || false,
-					offset = parseInt(req.query.offset, 10) || 0,
-					search = req.query.search || false,
-					query = dbCon.knex(dbTable).column(dbColumns).where(filter);
-
-				if (limit) {
-					query.limit(limit).offset(offset);
-				}
-
-				if (search) {
-					search = search.trim();
-					if (search.length < minSearchCharacters) {
-						next(errorHandler.badRequestError([
-							'Search string should be at least ' + minSearchCharacters + ' long.'
-						]));
-					}
-					limit = limit ? Math.min(searchLimit, limit) : searchLimit;
-					query.join('tile_set_search', 'tile_set.id', '=', 'tile_set_search.id');
-					query.whereRaw('MATCH (tile_set_search.name) AGAINST (?)', [search]);
-					query.limit(limit).offset(0);
-					query.orderBy('created_on', 'desc');
-				}
-				dbCon.getConnection(dbCon.connType.all, function (conn) {
-					query.connection(conn);
-					query.select().then(
-						function (collection) {
-							collection.forEach(function (value) {
-								value.image = utils.imageUrl(
-									config.dfsHost,
-									utils.getBucketName(
-										config.bucketPrefix + config.tileSetPrefix,
-										value.id
-									),
-									value.image
-								);
-								value.url = utils.responseUrl(req, utils.addTrailingSlash(req.route.path), value.id);
-							});
-
-							res.send(200, collection);
-							res.end();
-						},
-						next
-					);
-				}, next);
-			},
+			GET: getTileSets,
 			POST: function (req, res, next) {
 				var reqBody = reqBodyParser(req.rawBody),
 					errors = jsonValidator.validateJSON(reqBody, createSchema);
