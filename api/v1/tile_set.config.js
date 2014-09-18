3c2a9d35c2bb8a9835669509241a06c1f38bb44b
Rafal Leszczynski <rafal@wikia-inc.com>
2014-09-18 10:41:06 +0200

    move config variables to tile set config file

---
+++ api/v1/tile_set.config.js
@@ -0,0 +1,59 @@
+var jsonValidator = require('./../../lib/jsonValidator');
+
+module.exports = {
+	dbTable: 'tile_set',
+	getCollectionDbColumns: [
+		'tile_set.id',
+		'tile_set.name',
+		'tile_set.type',
+		'tile_set.status',
+		'tile_set.width',
+		'tile_set.height',
+		'tile_set.image'
+	],
+	getTileSetDbColumns: [
+		'name',
+		'type',
+		'image',
+		'width',
+		'height',
+		'min_zoom',
+		'max_zoom',
+		'status',
+		'created_by',
+		'created_on',
+		'attribution',
+		'subdomains'
+	],
+	createSchema: {
+		description: 'Schema for creating tile set',
+		type: 'Object',
+		properties: {
+			name: {
+				description: 'Tile set name',
+				type: 'string',
+				required: true,
+				minLength: 1,
+				maxLength: 255
+			},
+			url: {
+				description: 'URL to image from which tiles wil be created',
+				type: 'string',
+				pattern: jsonValidator.getUrlPattern(),
+				required: true,
+				maxLength: 255
+			},
+			created_by: {
+				description: 'Creator user name',
+				type: 'string',
+				required: true,
+				minLength: 1,
+				maxLength: 255
+			}
+		},
+		additionalProperties: false
+	},
+	searchLimit: 50,
+	minSearchCharacters: 2,
+	searchErrorMsg: 'Search string should be at least ' + this.minSearchCharacters + ' long.'
+};
