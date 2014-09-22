04d508038df66fd2b7472dcd82b0e7f01b871dbc
Rafal Leszczynski <rafal@wikia-inc.com>
2014-09-22 09:44:52 +0200

    add pass error method to tile set utils

--- api/v1/tile_set.utils.js
+++ api/v1/tile_set.utils.js
@@ -1,8 +1,15 @@
 'use strict';
 
-var utils = require('./../../lib/utils'),
+var Q = require('q'),
+	utils = require('./../../lib/utils'),
 	config = require('./../../lib/config'),
 	tileSetConfig = require('./tile_set.config');
+
+function passError(error) {
+	var deferred = Q.defer().reject(error);
+
+	return deferred.promise;
+}
 
 /**
  * @desc creates tileSet image URL and returns it
@@ -127,6 +134,7 @@ function setupCreateTileSetResponse(dbRes, req) {
 }
 
 module.exports = {
+	passError: passError,
 	addSearchToQuery: addSearchToQuery,
 	validateSearchTerm: validateSearchTerm,
 	setupSearchLimit: setupSearchLimit,
