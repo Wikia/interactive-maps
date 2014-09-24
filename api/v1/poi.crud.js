2cec1437fc42697e94d2c1bb2bbee250af28533d
nAndy <nandy@wikia-inc.com>
2014-09-24 14:05:24 +0200

    moved helper functions to utils module

--- api/v1/poi.crud.js
+++ api/v1/poi.crud.js
@@ -5,118 +5,14 @@ var dbCon = require('./../../lib/db_connector'),
 	errorHandler = require('./../../lib/errorHandler'),
 	utils = require('./../../lib/utils'),
 	squidUpdate = require('./../../lib/squidUpdate'),
-	taskQueue = require('./../../lib/taskQueue'),
-	logger = require('./../../lib/logger'),
 	poiConfig = require('./poi.config'),
+	poiUtils = require('./poi.utils'),
 	jsonValidator = require('./../../lib/jsonValidator');
-
-/**
- * @desc Helper function to get map_id from poi_id
- *
- * @param {object} conn
- * @param {number} poiId
- * @returns {object}
- */
-function getMapIdByPoiId(conn, poiId) {
-	return dbCon.select(
-		conn,
-		poiConfig.dbTable,
-		['map_id'],
-		{
-			id: poiId
-		}
-	);
-}
-
-/**
- * @desc Collect poi data for search indexing
- *
- * @param {object} conn Database connection
- * @param {number} poiId Poi id
- * @returns {object}
- */
-function collectPoiData(conn, poiId) {
-	return dbCon.knex(poiConfig.dbTable)
-		.select(
-			'poi.id',
-			'poi.name',
-			'poi.description',
-			'poi.link',
-			'poi.photo',
-			'poi.lat',
-			'poi.lon',
-			dbCon.raw('UNIX_TIMESTAMP(poi.created_on) AS created_on'),
-			'poi.created_by',
-			dbCon.raw('UNIX_TIMESTAMP(poi.updated_on) AS updated_on'),
-			'poi.updated_by',
-			'poi.link_title',
-			'poi.poi_category_id',
-			'poi_category.name AS poi_category_name',
-			'poi_category.parent_poi_category_id',
-			'poi.map_id',
-			'map.city_id',
-			'map.tile_set_id',
-			dbCon.raw('UNIX_TIMESTAMP(map.created_on) AS map_created_on'),
-			'map.created_by AS map_created_by'
-		)
-		.join('poi_category', 'poi.poi_category_id', '=', 'poi_category.id')
-		.join('map', 'poi.map_id', '=', 'map.id')
-		.connection(conn)
-		.where('poi.id', poiId);
-}
-
-/**
- * @desc Send poi data to search processing queue
- *
- * @param {object} conn Database connection
- * @param {string} operation POI Operation defined in poiOperations
- * @param {number} poiId Poi id
- */
-function addPoiDataToQueue(conn, operation, poiId) {
-	var workId = operation + poiId,
-		context;
-	if (operation === poiConfig.poiOperations.delete) {
-		context = {
-			operation: operation,
-			data: [
-				{
-					id: poiId
-				}
-			]
-		};
-		taskQueue.publish(taskQueue.payload(
-			taskQueue.tasks.poiUpdate,
-			'',
-			workId,
-			context
-		));
-	} else {
-		collectPoiData(conn, poiId).then(function (poiData) {
-			var row;
-			if (poiData.length > 0) {
-				row = poiData[0];
-				context = {
-					operation: operation,
-					data: poiData
-				};
-				taskQueue.publish(taskQueue.payload(
-					taskQueue.tasks.poiUpdate,
-					poiConfig.poiOperations.insert === operation ? row.created_by : row.updated_by,
-					workId,
-					context
-				));
-			} else {
-				logger.error('POI ' + poiId + ' not found in database');
-			}
-		});
-	}
-}
 
 /**
  * @desc Creates CRUD collection based on configuration object passed as parameter
  * @returns {object} - CRUD collection
  */
-
 module.exports = function createCRUD() {
 	return {
 		handler: {
@@ -157,7 +53,7 @@ module.exports = function createCRUD() {
 									utils.changeMapUpdatedOn(conn, dbCon, mapId).then(
 										function () {
 											squidUpdate.purgeKey(utils.surrogateKeyPrefix + mapId, 'mapPoiCreated');
-											addPoiDataToQueue(conn, poiConfig.poiOperations.insert, id);
+											poiUtils.addPoiDataToQueue(conn, poiConfig.poiOperations.insert, id);
 											res.send(201, response);
 											res.end();
 										},
@@ -180,7 +76,7 @@ module.exports = function createCRUD() {
 					};
 				if (isFinite(id)) {
 					dbCon.getConnection(dbCon.connType.master, function (conn) {
-						getMapIdByPoiId(conn, id).then(
+						poiUtils.getMapIdByPoiId(conn, id).then(
 							function (rows) {
 								if (rows.length > 0) {
 									var mapId = rows[0].map_id;
@@ -195,7 +91,7 @@ module.exports = function createCRUD() {
 															utils.surrogateKeyPrefix + mapId,
 															'mapPoiDeleted'
 														);
-														addPoiDataToQueue(conn, poiConfig.poiOperations.delete, id);
+														poiUtils.addPoiDataToQueue(conn, poiConfig.poiOperations.delete, id);
 														res.send(204, {});
 														res.end();
 													},
@@ -259,7 +155,7 @@ module.exports = function createCRUD() {
 
 					if (isFinite(id)) {
 						dbCon.getConnection(dbCon.connType.master, function (conn) {
-							getMapIdByPoiId(conn, id).then(
+							poiUtils.getMapIdByPoiId(conn, id).then(
 								function (rows) {
 									if (rows.length > 0) {
 										mapId = rows[0].map_id;
@@ -278,7 +174,7 @@ module.exports = function createCRUD() {
 																utils.surrogateKeyPrefix + mapId,
 																'mapPoiUpdated'
 															);
-															addPoiDataToQueue(conn, poiConfig.poiOperations.update, id);
+															poiUtils.addPoiDataToQueue(conn, poiConfig.poiOperations.update, id);
 															res.send(303, response);
 															res.end();
 														},
