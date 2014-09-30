95aa48e5ee800b822e7711a60b551c6ff8a7f1d1
nAndy <nandy@wikia-inc.com>
2014-09-30 15:37:31 +0200

    refactored poi.utils.js unit tests and added one more test

--- specs/poi.utils-spec.js
+++ specs/poi.utils-spec.js
@@ -10,18 +10,51 @@ var proxyquire = require('proxyquire').noCallThru(),
 	},
 	taskQueueStub = {},
 	loggerStub = {},
-	poiConfig = {
-		dbTable: {}
+	poiConfigStub = {
+		poiOperations: {
+			'delete': 'delete',
+			'insert': 'insert',
+			'update': 'update'
+		}
 	},
-	poiCrudUtils = proxyquire('../api/v1/poi.utils', {
+	poiCrudUtils;
+
+function getPoiCrudUtilsMock(dbConStub, taskQueueStub, loggerStub, poiConfigStub) {
+	return proxyquire('../api/v1/poi.utils', {
 		'./../../lib/db_connector': dbConStub,
 		'./../../lib/taskQueue': taskQueueStub,
 		'./../../lib/logger': loggerStub,
-		'./poi.config': poiConfig
+		'./poi.config': poiConfigStub
 	});
+}
 
 describe('poi.utils.js', function () {
-	it('getMapIdByPoiId() passes proper where options', function () {
+	it('passes proper where options while executing getMapIdByPoiId()', function () {
+		poiCrudUtils = getPoiCrudUtilsMock(dbConStub, taskQueueStub, loggerStub, poiConfigStub);
 		poiCrudUtils.getMapIdByPoiId({}, 123);
+	});
+
+	it('works properly while executing addPoiDataToQueue() for POI deletion', function () {
+		var taskQueueStub = {
+			tasks: {
+				poiUpdate: 'update'
+			},
+			publish: function () {},
+			payload: function (taskType, createdBy, workId, context) {
+				expect(createdBy).toEqual('');
+				expect(workId).toEqual('delete' + 123);
+				expect(context).toEqual({
+					operation: 'delete',
+					data: [
+						{
+							id: 123
+						}
+					]
+				});
+			}
+		};
+
+		poiCrudUtils = getPoiCrudUtilsMock(dbConStub, taskQueueStub, loggerStub, poiConfigStub);
+		poiCrudUtils.addPoiDataToQueue({}, poiConfigStub.poiOperations.delete, 123);
 	});
 });
