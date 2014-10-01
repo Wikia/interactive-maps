d8df5a71526e5541ec1e23dd77b3ed37b837daf5
nAndy <nandy@wikia-inc.com>
2014-10-01 15:22:13 +0200

    refactored poi.utils.js unit tests

--- specs/poi.utils-spec.js
+++ specs/poi.utils-spec.js
@@ -1,31 +1,23 @@
 'use strict';
 
-var proxyquire = require('proxyquire').noCallThru(),
-	dbConStub = {
-		select: function (conn, table, fields, where) {
-			expect(where).toEqual({
-				id: 123
-			});
-		}
-	},
-	taskQueueStub = {},
-	loggerStub = {},
-	poiConfigStub = {
-		poiOperations: {
-			'delete': 'delete',
-			'insert': 'insert',
-			'update': 'update'
-		}
-	},
-	poiCrudUtils = proxyquire('../api/v1/poi.utils', {
-		'./../../lib/db_connector': dbConStub,
-		'./../../lib/taskQueue': taskQueueStub,
-		'./../../lib/logger': loggerStub,
-		'./poi.config': poiConfigStub
-	});
+var proxyquire = require('proxyquire').noCallThru();
 
 describe('poi.utils.js', function () {
 	it('passes proper where options while executing getMapIdByPoiId()', function () {
+		var dbConStub = {
+				select: function (conn, table, fields, where) {
+					expect(where).toEqual({
+						id: 123
+					});
+				}
+			},
+			poiCrudUtils = proxyquire('../api/v1/poi.utils', {
+				'./../../lib/db_connector': dbConStub,
+				'./../../lib/taskQueue': {},
+				'./../../lib/logger': {},
+				'./poi.config': {}
+			});
+
 		poiCrudUtils.getMapIdByPoiId({}, 123);
 	});
 });
