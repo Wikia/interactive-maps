89dad1adf863ca5b1dadb48f9551539b82fe90ee
bkoval <brt.kowal@gmail.com>
2014-10-09 14:29:27 +0200

    Flattening promise chain in crud file

--- api/v1/map_data.crud.js
+++ api/v1/map_data.crud.js
@@ -13,13 +13,12 @@ var dbCon = require('./../../lib/db_connector'),
 function loadData(conn, mapId) {
 	return mapDataLoader
 		.getRawMapInfo(conn, mapId)
-		.then(function(mapData){
+		.then(function(mapData) {
 			mapData = mapData[0];
-			return mapDataLoader
-				.getPois(conn, mapData, true)
-				.then(function(mapData) {
-					return mapDataLoader.getPoiCategories(conn, mapData, true);
-				});
+			return mapDataLoader.getPois(conn, mapData, true);
+		})
+		.then(function(mapData) {
+			return mapDataLoader.getPoiCategories(conn, mapData, true);
 		});
 }
 
