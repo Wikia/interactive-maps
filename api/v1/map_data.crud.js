f5dd607cab081834bc1f00270dfcbab99c21d437
bkoval <brt.kowal@gmail.com>
2014-10-09 13:21:21 +0200

    Adding new lines and rearranging dots

--- api/v1/map_data.crud.js
+++ api/v1/map_data.crud.js
@@ -11,10 +11,12 @@ var dbCon = require('./../../lib/db_connector'),
  * @returns {object} promise
  */
 function loadData(conn, mapId) {
-	return mapDataLoader.getRawMapInfo(conn, mapId)
+	return mapDataLoader
+		.getRawMapInfo(conn, mapId)
 		.then(function(mapData){
 			mapData = mapData[0];
-			return mapDataLoader.getPois(conn, mapData, true)
+			return mapDataLoader
+				.getPois(conn, mapData, true)
 				.then(function(mapData) {
 					return mapDataLoader.getPoiCategories(conn, mapData, true);
 				});
