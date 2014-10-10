c77796e8075d91fa5f6a2ee746b0cb500856f0a0
bkoval <brt.kowal@gmail.com>
2014-10-10 15:03:41 +0200

    Rearranging file structure and reusing map_data methods

---
+++ api/v1/map_data.config.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+	mapColumns: [
+		'id',
+		'title',
+		'city_id',
+		'deleted'
+	],
+	poiColumns: [
+		'id',
+		'name',
+		'link',
+		'link_title',
+		'description',
+		'poi_category_id'
+	],
+	poiCategoryColumns: [
+		'id',
+		'name'
+	],
+};
--- api/v1/map_data.utils.js
+++ api/v1/map_data.utils.js
@@ -1,6 +1,10 @@
 'use strict';
 
-var mapDataLoader = require('./../../lib/mapDataLoader');
+var mapDataConfig = require('./map_data.config'),
+	dbCon = require('./../../lib/db_connector'),
+	utils = require('./../../lib/utils'),
+	config = require('./../../lib/config'),
+	Q = require('q');
 
 /**
  * @desc Loads map data from DB
@@ -9,17 +13,140 @@ var mapDataLoader = require('./../../lib/mapDataLoader');
  * @returns {object} promise
  */
 function loadData(conn, mapId) {
-	return mapDataLoader
-		.getRawMapInfo(conn, mapId)
+	return getMapInfo(conn, mapId)
 		.then(function(mapData) {
 			mapData = mapData[0];
-			return mapDataLoader.getPois(conn, mapData, true);
+			return getPois(conn, mapData, mapDataConfig.poiColumns);
 		})
 		.then(function(mapData) {
-			return mapDataLoader.getPoiCategories(conn, mapData, true);
+			return getPoiCategories(conn, mapData, mapDataConfig.poiCategoryColumns);
 		});
 }
 
+/**
+ * @desc Fetches basic map information from DB
+ * @param {object} conn Database connection
+ * @param {number} mapId
+ * @returns {object} promise
+ */
+function getMapInfo(conn, mapId) {
+	return dbCon.knex('map')
+		.column(mapDataConfig.mapColumns)
+		.where('map.id', '=', mapId)
+		.connection(conn)
+		.select();
+}
+
+/**
+ * @desc Load Points for map instance
+ * @param {object} conn Database connection
+ * @param {number} mapId
+ * @param {Array} columns DB columns to return
+ * @returns {object} - promise
+ */
+function loadPois(conn, mapId, columns) {
+	return dbCon.select(
+		conn,
+		'poi', columns, {
+			map_id: mapId
+		}
+	).then(escapeHTML);
+}
+
+/**
+ * @desc Escapes HTML in the poi names and descriptions
+ * @param {Array} collection
+ * @returns {Array}
+ */
+function escapeHTML(collection) {
+	collection.forEach(function (item) {
+		item.name = utils.escapeHtml(item.name);
+
+		if (item.description) {
+			item.description = utils.escapeHtml(item.description);
+		}
+	});
+	return collection;
+}
+
+/**
+ * @desc Load point types
+ * @param {object} conn Database connection
+ * @param {number} mapId
+ * @param {Array} columns DB columns to return
+ * @returns {object} - promise
+ */
+function loadPoiCategories(conn, mapId, columns) {
+	return dbCon.select(
+		conn,
+		'poi_category',
+		columns,
+		{
+			'map_id': mapId
+		}
+	).then(
+		function (collection) {
+			collection.forEach(function (item) {
+				item.name = utils.escapeHtml(item.name);
+			});
+			return collection;
+		}
+	);
+}
+
+/**
+ * @desc Get points
+ * @param {object} conn Database connection
+ * @param {object} mapData
+ * @param {Array} columns DB columns to return
+ * @returns {object} - promise
+ */
+function getPois(conn, mapData, columns) {
+	var deferred = Q.defer();
+	loadPois(conn, mapData.id, columns)
+		.then(
+		function (points) {
+			mapData.pois = points;
+			deferred.resolve(mapData);
+		},
+		function (error) {
+			deferred.reject({
+				code: 500,
+				message: error
+			});
+		}
+	);
+	return deferred.promise;
+}
+
+/**
+ * @desc Gets points types for map instance
+ * @param {object} conn Database connection
+ * @param {object} mapData
+ * @param {Array} columns DB columns to return
+ * @returns {object} - promise
+ */
+function getPoiCategories(conn, mapData, columns) {
+	var deferred = Q.defer();
+
+	loadPoiCategories(conn, mapData.id, columns).then(
+		function (poiCategories) {
+			mapData.poi_categories = poiCategories;
+			deferred.resolve(mapData);
+		},
+		function (error) {
+			deferred.reject({
+				code: 500,
+				message: error
+			});
+		}
+	);
+
+	return deferred.promise;
+}
+
 module.exports = {
-	loadData: loadData
+	loadData: loadData,
+	getPois: getPois,
+	getPoiCategories: getPoiCategories
 }
