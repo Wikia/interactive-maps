bf9321b80a02741b48889179aec3f8e185bade99
bkoval <brt.kowal@gmail.com>
2014-10-13 17:18:36 +0200

    Handling not founds

--- api/v1/map_data.crud.js
+++ api/v1/map_data.crud.js
@@ -3,6 +3,7 @@
 var dbCon = require('./../../lib/db_connector'),
 	mapDataUtils = require('./map_data.utils'),
 	mapDataConfig = require('./map_data.config'),
+	errorHandler = require('./../../lib/errorHandler'),
 	utils = require('./../../lib/utils');
 /**
  * @desc Entry point handler for extracting metadata associated with the map
@@ -12,26 +13,27 @@ var dbCon = require('./../../lib/db_connector'),
 function getMapData(req, res) {
 	var mapId = parseInt(req.pathVar.id, 10) || 0,
 		conn;
-	if (mapId !== 0) {
-		dbCon
-			.getConnection(dbCon.connType.all)
-			.then(function(connection){
-				conn = connection;
-				return mapDataUtils.getMapInfo(conn, mapId);
-			})
-			.then(function(mapData) {
-				mapData = mapData[0];
-				return mapDataUtils
-					.getPois(conn, mapData, mapDataConfig.poiColumns);
-			})
-			.then(function(mapData) {
-				return mapDataUtils
-					.getPoiCategories(conn, mapData, mapDataConfig.poiCategoryColumns);
-			})
-			.then(function (mapData) {
-				utils.sendHttpResponse(res, 200, mapData);
-			});
+	if (mapId === 0) {
+		errorHandler.serveNotFoundError(req, res);
 	}
+	dbCon
+		.getConnection(dbCon.connType.all)
+		.then(function(connection) {
+			conn = connection;
+			return mapDataUtils.getMapInfo(conn, mapId);
+		})
+		.then(function(mapData) {
+			mapData = mapData[0];
+			return mapDataUtils
+				.getPois(conn, mapData, mapDataConfig.poiColumns);
+		})
+		.then(function(mapData) {
+			return mapDataUtils
+				.getPoiCategories(conn, mapData, mapDataConfig.poiCategoryColumns);
+		})
+		.then(function (mapData) {
+			utils.sendHttpResponse(res, 200, mapData);
+		});
 }
 
 /**
@@ -40,7 +42,9 @@ function getMapData(req, res) {
  */
 module.exports = function createCRUD() {
 	return {
-		handler: {},
+		handler: {
+			GET: errorHandler.serveNotFoundError
+		},
 		wildcard: {
 			GET: getMapData
 		}
