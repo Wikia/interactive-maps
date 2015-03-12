edb1747208c59434ac78a18a5a876673948dc004
Rafal Leszczynski <rafal@wikia-inc.com>
2015-03-12 15:31:56 +0100

    move conditional logic for article links to separate function

--- assets/scripts/im.pontoWikiaAppsBridge.js
+++ assets/scripts/im.pontoWikiaAppsBridge.js
@@ -17,6 +17,16 @@ define('im.pontoWikiaAppsBridge', ['ponto', 'im.window', 'im.config'], function 
 	}
 
 	/**
+	 * @desc checks if target is valid link to article
+	 * @param {Element} target - HTML node element
+	 * @returns {boolean}
+	 */
+	function isLinkToArticle(target) {
+		return (target.tagName === 'A' && target.className.indexOf(config.articleLinkClassName) !== -1) ||
+			(target.tagName === 'IMG' && target.parentNode.className.indexOf(config.articleLinkClassName) !== -1);
+	}
+
+	/**
 	 * @desc prevents default link behavior and sends supported links to native wikia app for processing
 	 */
 	function setupArticleLinks() {
@@ -25,13 +35,7 @@ define('im.pontoWikiaAppsBridge', ['ponto', 'im.window', 'im.config'], function 
 		body.addEventListener('click', function (event) {
 			var target = event.target;
 
-			if (
-				target.tagName === 'A' &&
-				target.className.indexOf(config.articleLinkClassName) !== -1 ||
-				target.tagName === 'IMG') &&
-				target.parentNode.className.indexOf(config.articleLinkClassName) !== -1
-			) {
-
+			if (isLinkToArticle(target)) {
 				event.preventDefault();
 				sendLinkUrl(target);
 			}
